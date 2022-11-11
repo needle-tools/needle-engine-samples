961173aa32f57283a377c018b9ecd4a1533c5470
Marcel Wiessler <marcel@gaisterhand.de>
2022-11-11 15:36:47 +0100

    Add click particle burst effect

---
+++ assets/Runtime/_Shared/SampleScripts.codegen/EmitParticlesOnClick.cs
@@ -0,0 +1,15 @@
+// NEEDLE_CODEGEN_START
+// auto generated code - do not edit directly
+
+#pragma warning disable
+
+namespace Needle.Typescript.GeneratedComponents
+{
+	public partial class EmitParticlesOnClick : UnityEngine.MonoBehaviour
+	{
+		public UnityEngine.ParticleSystem[] @particleSystems = new UnityEngine.ParticleSystem[]{ };
+		public void awake(){}
+	}
+}
+
+// NEEDLE_CODEGEN_END
