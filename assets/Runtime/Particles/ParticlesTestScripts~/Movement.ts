316fab2caccc115bed6a637910dccb69b69278c8
Marcel Wiessler <marcel@gaisterhand.de>
2022-11-14 13:07:09 +0100

    Particles rotate

--- assets/Runtime/Particles/ParticlesTestScripts~/Movement.ts
+++ assets/Runtime/Particles/ParticlesTestScripts~/Movement.ts
@@ -1,4 +1,5 @@
-import { Behaviour, serializeable } from "@needle-tools/engine";
+import { Behaviour, serializable, serializeable } from "@needle-tools/engine";
+import { Vector3, AxesHelper } from "three";
 
 export class Particle_MoveAround extends Behaviour {
 
@@ -9,4 +10,14 @@ export class Particle_MoveAround extends Behaviour {
         this.gameObject.position.y += Math.sin(this.context.time.time) * this.context.time.deltaTime * this.factor;
     }
 
+}
+
+export class Particle_Rotate extends Behaviour
+{
+    @serializable(Vector3)
+    speed : Vector3 = new Vector3(1, 0, 0);
+
+    update(){
+        this.gameObject.rotateOnAxis(this.speed, this.context.time.deltaTime);
+    }
 }
--- assets/Runtime/Particles/ParticlesTestScripts~/codegen/register_types.js
+++ assets/Runtime/Particles/ParticlesTestScripts~/codegen/register_types.js
@@ -2,6 +2,8 @@
 
 // Import types
 import { Particle_MoveAround } from "../Movement.ts";
+import { Particle_Rotate } from "../Movement.ts";
 
 // Register types
 TypeStore.add("Particle_MoveAround", Particle_MoveAround);
+TypeStore.add("Particle_Rotate", Particle_Rotate);
