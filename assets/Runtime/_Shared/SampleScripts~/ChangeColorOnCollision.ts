eefa2a364282d5672ffe7a7a82fc26b9343428ee
Marcel Wiessler <marcel@gaisterhand.de>
2022-11-14 16:06:55 +0100

    Basic physics: add objects without gravity

--- assets/Runtime/_Shared/SampleScripts~/ChangeColorOnCollision.ts
+++ assets/Runtime/_Shared/SampleScripts~/ChangeColorOnCollision.ts
@@ -1,15 +1,20 @@
 import { Behaviour, Collision, Renderer } from "@needle-tools/engine";
+import{ Color } from "three";
 
 export class ChangeColorOnCollision extends Behaviour {
 
     private renderer: Renderer | null = null;
     private collisionCount: number = 0;
 
+    private _startColor? : Color[];
+
     start() {
         this.renderer = this.gameObject.getComponent(Renderer);
         if (!this.renderer) return;
+        if(!this._startColor) this._startColor = [];
         for (let i = 0; i < this.renderer.sharedMaterials.length; i++) {
             this.renderer.sharedMaterials[i] = this.renderer.sharedMaterials[i].clone();
+            this._startColor[i] = this.renderer.sharedMaterials[i]["color"].clone();
         }
     }
 
@@ -22,11 +27,12 @@ export class ChangeColorOnCollision extends Behaviour {
     }
 
     onCollisionExit(_col: Collision) {
-        if (!this.renderer) return;
+        if (!this.renderer || !this._startColor) return;
         this.collisionCount -= 1;
         if (this.collisionCount === 0) {
             for (let i = 0; i < this.renderer.sharedMaterials.length; i++) {
-                this.renderer.sharedMaterials[i]["color"].setRGB(.1, .1, .1);
+                this.renderer.sharedMaterials[i]["color"].copy(this._startColor[i])
+                // .setRGB(.1, .1, .1);
             }
         }
     }
