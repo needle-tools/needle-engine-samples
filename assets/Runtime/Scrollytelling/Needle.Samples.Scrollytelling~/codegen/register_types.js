c39c7e045392af2ba762a1f2090028e2f778e125
Marcel Wiessler <marcel@gaisterhand.de>
2022-12-07 21:37:35 +0100

    Scrollytelling: add sligth camera movement

--- assets/Runtime/Scrollytelling/Needle.Samples.Scrollytelling~/codegen/register_types.js
+++ assets/Runtime/Scrollytelling/Needle.Samples.Scrollytelling~/codegen/register_types.js
@@ -1,7 +1,9 @@
 ﻿import { TypeStore } from "@needle-tools/engine/engine/engine_typestore"
 
 // Import types
+import { MouseRotation } from "../MouseRotation.ts";
 import { ScrollTimeline } from "../ScrollTimeline.ts";
 
 // Register types
+TypeStore.add("MouseRotation", MouseRotation);
 TypeStore.add("ScrollTimeline", ScrollTimeline);
