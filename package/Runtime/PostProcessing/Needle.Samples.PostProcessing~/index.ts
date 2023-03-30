3a9ef4abadf0ed77188a4c5c1c27a195f94c59e5
Marcel Wiessler <marcel@gaisterhand.de>
2023-03-30 15:13:50 +0200

    Update package + postprocessing scene

--- package/Runtime/PostProcessing/Needle.Samples.PostProcessing~/index.ts
+++ package/Runtime/PostProcessing/Needle.Samples.PostProcessing~/index.ts
@@ -1,6 +1,6 @@
 ﻿
 
-import { Behaviour, DepthOfField, Gizmos, serializable, Volume } from "@needle-tools/engine";
+import { Behaviour, DepthOfField, Gizmos, Mathf, serializable, Volume } from "@needle-tools/engine";
 import { RaycastOptions } from "@needle-tools/engine/src/engine/engine_physics";
 import { Vector2 } from "three";
 
@@ -21,16 +21,25 @@ export class FocusDistancer extends Behaviour {
                     let hits: any = undefined;
                     if (this.autoFocus) {
                         const rc = new RaycastOptions();
-                        rc.screenPoint = new Vector2(0,0);
+                        rc.screenPoint = new Vector2(0, 0);
                         hits = this.context.physics.raycast(rc);
                     }
                     else // raycasts at cursor position by default
                         hits = this.context.physics.raycast();
 
                     if (hits?.length) {
-                        Gizmos.DrawWireSphere(hits[0].point, .05, 0x5588dd);
+                        const gizmoDuration = this.autoFocus ? undefined : 1;
+                        Gizmos.DrawWireSphere(hits[0].point, .05, 0x5588dd, gizmoDuration);
+
                         const hitInCam = this.context.mainCamera?.worldToLocal(hits[0].point);
-                        ef.focusDistance.value = -hitInCam!.z;
+                        const targetFocus = -hitInCam!.z;
+                        if (this.autoFocus) {
+                            const t = this.context.time.deltaTime / .05;
+                            ef.focusDistance.value = Mathf.lerp(ef.focusDistance.value, targetFocus, t);
+                        }
+                        else {
+                            ef.focusDistance.value = targetFocus;
+                        }
                     }
                 }
             }
