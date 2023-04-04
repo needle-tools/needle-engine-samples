b06b688a4ce4fab7c9b9d785d5ffafefcd7542e8
Felix Herbst <felix.herbst@gmail.com>
2023-04-04 15:21:57 +0200

    added interactive map sample
    fix ts warning

---
+++ package/Runtime/InteractiveMap/Scripts/Needle.InteractiveMap.codegen/MapLocator.cs
@@ -0,0 +1,18 @@
+// NEEDLE_CODEGEN_START
+// auto generated code - do not edit directly
+
+#pragma warning disable
+
+namespace Needle.Typescript.GeneratedComponents
+{
+	public partial class MapLocator : UnityEngine.MonoBehaviour
+	{
+		public void OnEnable(){}
+		public void OnDisable(){}
+		public void searchLocation(object @evt){}
+		public void geoFindMe(){}
+		public void onBeforeRender(object @frame){}
+	}
+}
+
+// NEEDLE_CODEGEN_END
