34d01d03b1a3194d5405550c5ee9621a5ebbb8e1
Marcel Wiessler <marcel@gaisterhand.de>
2023-04-08 20:07:47 +0200

    test enabling webgpu

---
+++ package/Runtime/GPURenderer/gpu-rendering~/index.ts
@@ -0,0 +1,27 @@
+﻿import { Context, ContextEvent, ContextRegistry, showBalloonWarning } from "@needle-tools/engine";
+
+
+import WebGPU from 'three/examples/jsm/capabilities/WebGPU.js';
+import WebGPURenderer from 'three/examples/jsm/renderers/webgpu/WebGPURenderer.js';
+import { cubeTexture, texture, normalMap, toneMapping } from 'three/examples/jsm/nodes/Nodes.js';
+import { LinearToneMapping, sRGBEncoding } from 'three';
+
+ContextRegistry.registerCallback(ContextEvent.ContextCreated, (args) => {
+
+
+    if (WebGPU.isAvailable() === false) {
+        showBalloonWarning("WebGPU is not available");
+        return;
+    }
+
+    const renderer = new WebGPURenderer();
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.toneMappingNode = toneMapping(LinearToneMapping, 1);
+    renderer.outputEncoding = sRGBEncoding;
+    args.context.renderer = renderer;
+
+});
+
+function toneMapping(LinearToneMapping: number, arg1: unknown): any {
+}
