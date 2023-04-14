c28f9d2be3f898b893782e9ac660b9930793fb6b
Felix Herbst <felix.herbst@gmail.com>
2023-04-14 14:55:59 +0200

    fix runtime compilation with editor ifdefs

--- package/Runtime/Hotspots/Scripts/Editor/HotspotEditor.cs
+++ package/Runtime/Hotspots/Scripts/Editor/HotspotEditor.cs
@@ -1,6 +1,6 @@
+#if UNITY_EDITOR
+
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
@@ -84,3 +84,5 @@ namespace Needle.Typescript.GeneratedComponents
         }
     }
 }
+
+#endif
