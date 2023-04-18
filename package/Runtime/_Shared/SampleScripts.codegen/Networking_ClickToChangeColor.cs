094a6a294810cec4afaa425f8ba8a3c6bc284d90
Marcel Wiessler <marcel@gaisterhand.de>
2023-04-19 00:01:31 +0200

    add basic auto networking sample

---
+++ package/Runtime/_Shared/SampleScripts.codegen/Networking_ClickToChangeColor.cs
@@ -0,0 +1,16 @@
+// NEEDLE_CODEGEN_START
+// auto generated code - do not edit directly
+
+#pragma warning disable
+
+namespace Needle.Typescript.GeneratedComponents
+{
+	public partial class Networking_ClickToChangeColor : UnityEngine.MonoBehaviour
+	{
+		public UnityEngine.Color @color;
+		public void onPointerClick(UnityEngine.EventSystems.PointerEventData @_){}
+		public void OnEnable(){}
+	}
+}
+
+// NEEDLE_CODEGEN_END
