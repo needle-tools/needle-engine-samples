c73be4fc3b44248bce25b662e9e0c246996b038b
Krystof <kipash612@gmail.com>
2023-04-19 13:01:12 +0200

    remove material property configurator ; replace with material cfg

--- package/Runtime/Configurator/Scripts/Configurator~/Material-property-modules/PropertyModule.ts
+++
@@ -1,9 +0,0 @@
-import { Behaviour } from "@needle-tools/engine";
-import { Material } from "three";
-
-export class PropertyModule extends Behaviour {
-  public getSize(): number {
-    return 0;
-  }
-  public applyState(_material: Material, _index: number): void {}
-}
--- package/Runtime/Configurator/Scripts/Configurator~/codegen/register_types.js
+++ package/Runtime/Configurator/Scripts/Configurator~/codegen/register_types.js
@@ -5,21 +5,13 @@ import { ConfigurationElement } from "../ConfigurationElement";
 import { Configurator } from "../Configurator";
 import { ConfiguratorControls } from "../ConfiguratorControls";
 import { MaterialConfigurator } from "../MaterialConfigurator";
-import { MaterialPropertyConfigurator } from "../MaterialPropertyConfigurator";
 import { ObjectConfigurator } from "../ObjectConfigurator";
 import { SyncConfiguratorTransform } from "../SyncConfiguratorTransform";
-import { ColorPropertyModule } from "../Material-property-modules/ColorPropertyModule";
-import { PropertyModule } from "../Material-property-modules/PropertyModule";
-import { TexturePropertyModule } from "../Material-property-modules/TexturePropertyModule";
 
 // Register types
 TypeStore.add("ConfigurationElement", ConfigurationElement);
 TypeStore.add("Configurator", Configurator);
 TypeStore.add("ConfiguratorControls", ConfiguratorControls);
 TypeStore.add("MaterialConfigurator", MaterialConfigurator);
-TypeStore.add("MaterialPropertyConfigurator", MaterialPropertyConfigurator);
 TypeStore.add("ObjectConfigurator", ObjectConfigurator);
 TypeStore.add("SyncConfiguratorTransform", SyncConfiguratorTransform);
-TypeStore.add("ColorPropertyModule", ColorPropertyModule);
-TypeStore.add("PropertyModule", PropertyModule);
-TypeStore.add("TexturePropertyModule", TexturePropertyModule);
