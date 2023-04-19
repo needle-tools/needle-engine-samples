c996bc7ed1778e112745406249f7e1b4c3101a28
Marcel Wiessler <marcel@gaisterhand.de>
2023-04-19 17:22:19 +0200

    some more particle effects

--- package/Runtime/Scrollytelling/Needle.Samples.Scrollytelling~/codegen/register_types.js
+++ package/Runtime/Scrollytelling/Needle.Samples.Scrollytelling~/codegen/register_types.js
@@ -1,9 +1,11 @@
 ﻿import { TypeStore } from "@needle-tools/engine"
 
 // Import types
-import { MouseRotation } from "../MouseRotation.ts";
-import { ScrollTimeline } from "../ScrollTimeline.ts";
+import { MouseRotation } from "../MouseRotation";
+import { ScrollTimeline } from "../ScrollTimeline";
+import { ScrollTimeline_2 } from "../ScrollTimeline_2";
 
 // Register types
 TypeStore.add("MouseRotation", MouseRotation);
 TypeStore.add("ScrollTimeline", ScrollTimeline);
+TypeStore.add("ScrollTimeline_2", ScrollTimeline_2);
