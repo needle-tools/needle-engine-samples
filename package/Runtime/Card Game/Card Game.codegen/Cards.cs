0232a3cdc63eeed8e60a3dbb5ae8b04fc69ecd32
Marcel Wiessler <marcel@gaisterhand.de>
2023-05-09 15:34:04 +0200

    card game local engine + adding removing card dynamically

---
+++ package/Runtime/Card Game/Card Game.codegen/Cards.cs
@@ -0,0 +1,16 @@
+// NEEDLE_CODEGEN_START
+// auto generated code - do not edit directly
+
+#pragma warning disable
+
+namespace Needle.Typescript.GeneratedComponents
+{
+	public partial class Cards : UnityEngine.MonoBehaviour
+	{
+		public UnityEngine.Transform @prefab;
+		public UnityEngine.Transform @container;
+		public void awake(){}
+	}
+}
+
+// NEEDLE_CODEGEN_END
