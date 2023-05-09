0afe3720d8b736173b2e3a18070551bc1f2943eb
Kippy <not@telling.ever>
2023-05-09 14:36:19 +0200

    add transparent video

---
+++ package/Runtime/VideoPlayer/Scripts/Needle.Sample.VideoPlayer~/codegen/register_types.js
@@ -0,0 +1,7 @@
+﻿import { TypeStore } from "@needle-tools/engine"
+
+// Import types
+import { CombinedVideo } from "../CombinedVideo";
+
+// Register types
+TypeStore.add("CombinedVideo", CombinedVideo);
