e12f8b7c746989cfa0ccca13856d93798f1af54b
Marcel Wiessler <marcel@gaisterhand.de>
2023-05-10 22:14:44 +0200

    move cards in include dir

--- package/WebProjects~/Card Game/src/main.ts
+++ package/WebProjects~/Card Game/src/main.ts
@@ -6,15 +6,15 @@ import { CardModel, Deck } from "@needle-tools/card-game-sample"
 // insert custom cards
 
 Deck.onInitialize(deck => {
-    const armabee = Deck.createCard("./cards/Armabee.gltf", "./cards/Armabee.jpg");
+    const armabee = Deck.createCard("./include/cards/Armabee.gltf", "./include/cards/Armabee.jpg");
     armabee.idleAnimation = "Flying_Idle";
     
-    const ghost = Deck.createCard("./cards/Ghost.gltf", "./cards/Ghost.jpg");
+    const ghost = Deck.createCard("./include/cards/Ghost.gltf", "./include/cards/Ghost.jpg");
     ghost.idleAnimation = "Flying_Idle";
 
     // this gltf has some "non-standard" animation names
-    const glub = Deck.createCard("./cards/Glub.gltf", "./cards/Glub.jpg");
+    const glub = Deck.createCard("./include/cards/Glub.gltf", "./include/cards/Glub.jpg");
     glub.idleAnimation = "Flying_Idle";
     
-    const mushroomKing = Deck.createCard("./cards/MushroomKing.gltf", "./cards/MushroomKing.jpg");
+    const mushroomKing = Deck.createCard("./include/cards/MushroomKing.gltf", "./include/cards/MushroomKing.jpg");
 })
