16d2ac6bd45efe888b1307ca3d0e08f31c1403f9
Marcel Wiessler <marcel@gaisterhand.de>
2023-05-15 15:33:55 +0200

    Fix next dev mode running

--- package/WebProjects~/NextTemplate/next.config.js
+++ package/WebProjects~/NextTemplate/next.config.js
@@ -4,7 +4,7 @@ const webpack = require("webpack");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  transpilePackages: ["three", "peerjs"],
+  transpilePackages: ["three", "peerjs", "@needle-tools/engine", "three-mesh-ui"],
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     config.plugins.push(definePlugin);
     return config;
