9b717e5069a14b8c78afb4bbbe945a38365b3ad4
Marcel Wiessler <marcel@gaisterhand.de>
2023-05-16 15:02:30 +0200

    minimal next config

--- package/WebProjects~/NextTemplate/next.config.js
+++ package/WebProjects~/NextTemplate/next.config.js
@@ -1,19 +1,7 @@
 const webpack = require("webpack");
 
 module.exports = async () => {
-  /** @type {import('next').NextConfig} */
-  const nextConfig = {
-    reactStrictMode: true
-  }
-
-  const { needleNext } = await import("@needle-tools/engine/plugins/webpack/index.js");
-  const finalConfig = Object.assign(nextConfig,
-    needleNext(nextConfig, {
-      modules: {
-        webpack
-      }
-    })
-  );
-  return finalConfig;
+  const { needleNext } = await import("@needle-tools/engine/plugins/next/index.js");
+  return needleNext({}, { modules: { webpack } });
 }
 
