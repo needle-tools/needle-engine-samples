d901a9711fc9644ab800caed6c7369c9da8cb94b
Marcel Wiessler <marcel@gaisterhand.de>
2023-06-26 13:56:01 +0200

    Fix: PassThrough sample buttons fix

--- package/Runtime/PresencePlatform/Needle.Samples.QuestPro~/PresencePlatformUI.ts
+++ package/Runtime/PresencePlatform/Needle.Samples.QuestPro~/PresencePlatformUI.ts
@@ -6,6 +6,63 @@ import { isQuest } from "@needle-tools/engine";
 export class PresencePlatformUI extends Behaviour {
 
     start() {
+
+        const div = this.context.domElement.shadowRoot!.querySelector(".webxr-buttons") as HTMLElement;
+        if (!div) {
+            console.warn("Could not find webxr-buttons element");
+            return;
+        }
+
+        this.appendWebXRButtonsStyle();
+
+        const launchOnQuest = document.createElement("button");
+        launchOnQuest.className = "webxr-button";
+        launchOnQuest.innerHTML = " 🎁 Launch</br>on Quest";
+        launchOnQuest.onclick = () => {
+            window.open("https://www.oculus.com/open_url/?url=" + document.URL, "_blank");
+        };
+
+        const xr = GameObject.findObjectOfType(WebXR)!;
+
+        const haveAR = WebXR.IsARSupported;
+        const haveVR = WebXR.IsVRSupported;
+
+        const startAR = WebXR.createARButton(xr);
+        const startVR = WebXR.createVRButton(xr);
+
+        const arButton = document.createElement("button")!;
+        if (isQuest())
+            arButton.innerHTML = "Passthrough";
+        else
+            arButton.innerHTML = "AR";
+        if (haveAR)
+            arButton.addEventListener("click", () => {
+                startAR.click();
+            });
+        if (haveAR) arButton.classList.add("supported");
+        else arButton.classList.add("not-supported");
+
+        const vrButton = document.createElement("button")!;
+        vrButton.innerHTML = "VR";
+        if (haveVR)
+            vrButton.addEventListener("click", () => {
+                startVR.click();
+            });
+        if (haveVR) vrButton.classList.add("supported");
+        else vrButton.classList.add("not-supported");
+
+        const info = document.createElement("span");
+        info.innerHTML = "Make sure to complete</br>Room Setup with walls.";
+
+        if (isQuest())
+            div.appendChild(info);
+        div.appendChild(arButton);
+        div.appendChild(vrButton);
+        if (!isQuest())
+            div.appendChild(launchOnQuest);
+    }
+
+    private appendWebXRButtonsStyle() {
         const style = document.createElement("style");
         style.innerHTML = `
         needle-engine .webxr-buttons {
@@ -39,53 +96,7 @@ export class PresencePlatformUI extends Behaviour {
             font-size: 30px;
         }
         `;
-        document.head.appendChild(style);
+        this.context.domElement.shadowRoot!.appendChild(style);
 
-        const div = document.getElementsByClassName("webxr-buttons")[0];
-        const launchOnQuest = document.createElement("button");
-        launchOnQuest.className = "webxr-button";
-        launchOnQuest.innerHTML = " 🎁 Launch</br>on Quest";
-        launchOnQuest.onclick = () => {
-            window.open("https://www.oculus.com/open_url/?url=" + document.URL, "_blank");
-        };
-
-        const xr = GameObject.findObjectOfType(WebXR)!;
-        
-        const haveAR = WebXR.IsARSupported;
-        const haveVR = WebXR.IsVRSupported;
-
-        const startAR = WebXR.createARButton(xr);
-        const startVR = WebXR.createVRButton(xr);
-
-        const arButton = document.createElement("button")!;
-        if (isQuest())
-            arButton.innerHTML = "Passthrough";
-        else
-            arButton.innerHTML = "AR";
-        if (haveAR)
-            arButton.addEventListener("click", () => { 
-                startAR.click();
-            });
-        if (haveAR) arButton.classList.add("supported");
-        else arButton.classList.add("not-supported");
-
-        const vrButton = document.createElement("button")!;
-        vrButton.innerHTML = "VR";
-        if (haveVR)
-            vrButton.addEventListener("click", () => { 
-                startVR.click();
-            });
-        if (haveVR) vrButton.classList.add("supported");
-        else vrButton.classList.add("not-supported");
-
-        const info = document.createElement("span");
-        info.innerHTML = "Make sure to complete</br>Room Setup with walls.";
-        
-        if (isQuest())
-            div.appendChild(info);
-        div.appendChild(arButton);
-        div.appendChild(vrButton);
-        if (!isQuest())
-            div.appendChild(launchOnQuest);
     }
 }
