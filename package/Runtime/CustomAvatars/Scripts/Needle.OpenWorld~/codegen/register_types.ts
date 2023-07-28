98bb1ad2c50d8b83c4dfdf758024892f90e582ac
Felix Herbst <felix.herbst@gmail.com>
2023-07-28 22:28:47 +0200

    bump versions

---
+++ package/Runtime/CustomAvatars/Scripts/Needle.OpenWorld~/codegen/register_types.ts
@@ -0,0 +1,13 @@
+﻿import { TypeStore } from "@needle-tools/engine"
+
+// Import types
+import { CharacterManager } from "../CharacterManager.js";
+import { CharacterSwitcher } from "../CharacterSwitcher.js";
+import { LocalPlayerControls } from "../LocalPlayerControls.js";
+import { SyncedAnimator } from "../SyncedAnimator.js";
+
+// Register types
+TypeStore.add("CharacterManager", CharacterManager);
+TypeStore.add("CharacterSwitcher", CharacterSwitcher);
+TypeStore.add("LocalPlayerControls", LocalPlayerControls);
+TypeStore.add("SyncedAnimator", SyncedAnimator);
