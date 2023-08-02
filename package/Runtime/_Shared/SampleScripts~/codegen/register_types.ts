99be5f1367a3dc93b51c012b77952b372b0b545d
Krystof <kipash612@gmail.com>
2023-08-02 19:08:44 +0200

    fix SyncedAnimator scene ref ; add PlayerStateUI and SyncedRoomUI to FPC sample ; redeploy

---
+++ package/Runtime/_Shared/SampleScripts~/codegen/register_types.ts
@@ -0,0 +1,63 @@
+﻿import { TypeStore } from "@needle-tools/engine"
+
+// Import types
+import { Cannon } from "../Cannon.js";
+import { RandomColor } from "../ChangeColor.js";
+import { ChangeColorOnCollision } from "../ChangeColorOnCollision.js";
+import { IncreaseShaderSpeedOverTime } from "../ChangeCustomShaderProperty.js";
+import { DisableEnvironmentLight } from "../DisableEnvironment.js";
+import { EmitParticlesOnClick } from "../EmitParticlesOnClick.js";
+import { HTMLButtonClick } from "../HTMLButtonEvent.js";
+import { IFrameContent } from "../IFrameContent.js";
+import { ImageTrackingDownloadUI } from "../ImageTrackingDownloadUI.js";
+import { LookAtCamera } from "../LookAtCamera.js";
+import { Networking_ClickToChangeColor } from "../Networking.js";
+import { Networking_StringArray } from "../Networking.js";
+import { Networking_Object } from "../Networking.js";
+import { PhysicsCollision } from "../PhysicsCollision.js";
+import { PhysicsTrigger } from "../PhysicsCollision.js";
+import { PlayAnimationOnCollision } from "../PlayAnimationOnCollision.js";
+import { PlayAnimationOnTrigger } from "../PlayAnimationOnTrigger.js";
+import { PlayAudioOnCollision } from "../PlayAudioOnCollision.js";
+import { PrefabSceneSwitcherSample } from "../SceneSwitcher.js";
+import { SceneSwitcherSample } from "../SceneSwitcher.js";
+import { ShowBalloonMessage } from "../ShowBalloonMessage.js";
+import { StartPosition } from "../StartPosition.js";
+import { AutoReset } from "../StartPosition.js";
+import { SyncedRoomUI } from "../SyncedRoomUI.js";
+import { TimedSpawn } from "../TimedSpawn.js";
+import { Variatns } from "../Variants.js";
+import { VideoBackground } from "../VideoBackground.js";
+import { ResetPositionOnInterval } from "../VisibilitySamples.js";
+import { ToggleVisibility } from "../VisibilitySamples.js";
+
+// Register types
+TypeStore.add("Cannon", Cannon);
+TypeStore.add("RandomColor", RandomColor);
+TypeStore.add("ChangeColorOnCollision", ChangeColorOnCollision);
+TypeStore.add("IncreaseShaderSpeedOverTime", IncreaseShaderSpeedOverTime);
+TypeStore.add("DisableEnvironmentLight", DisableEnvironmentLight);
+TypeStore.add("EmitParticlesOnClick", EmitParticlesOnClick);
+TypeStore.add("HTMLButtonClick", HTMLButtonClick);
+TypeStore.add("IFrameContent", IFrameContent);
+TypeStore.add("ImageTrackingDownloadUI", ImageTrackingDownloadUI);
+TypeStore.add("LookAtCamera", LookAtCamera);
+TypeStore.add("Networking_ClickToChangeColor", Networking_ClickToChangeColor);
+TypeStore.add("Networking_StringArray", Networking_StringArray);
+TypeStore.add("Networking_Object", Networking_Object);
+TypeStore.add("PhysicsCollision", PhysicsCollision);
+TypeStore.add("PhysicsTrigger", PhysicsTrigger);
+TypeStore.add("PlayAnimationOnCollision", PlayAnimationOnCollision);
+TypeStore.add("PlayAnimationOnTrigger", PlayAnimationOnTrigger);
+TypeStore.add("PlayAudioOnCollision", PlayAudioOnCollision);
+TypeStore.add("PrefabSceneSwitcherSample", PrefabSceneSwitcherSample);
+TypeStore.add("SceneSwitcherSample", SceneSwitcherSample);
+TypeStore.add("ShowBalloonMessage", ShowBalloonMessage);
+TypeStore.add("StartPosition", StartPosition);
+TypeStore.add("AutoReset", AutoReset);
+TypeStore.add("SyncedRoomUI", SyncedRoomUI);
+TypeStore.add("TimedSpawn", TimedSpawn);
+TypeStore.add("Variatns", Variatns);
+TypeStore.add("VideoBackground", VideoBackground);
+TypeStore.add("ResetPositionOnInterval", ResetPositionOnInterval);
+TypeStore.add("ToggleVisibility", ToggleVisibility);
