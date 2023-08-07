9e1545562054ecce675acef0cc72b1367a6cb33f
Krystof <kipash612@gmail.com>
2023-08-07 14:15:06 +0200

    force color space ; remove old code

--- package/Runtime/VideoPlayer/Scripts/Needle.Sample.VideoPlayer~/CombinedVideo.ts
+++ package/Runtime/VideoPlayer/Scripts/Needle.Sample.VideoPlayer~/CombinedVideo.ts
@@ -1,6 +1,5 @@
 import { Behaviour, FrameEvent, GameObject, Renderer, VideoPlayer, serializable } from "@needle-tools/engine";
-import { debug } from "@needle-tools/engine/lib/engine-components/ReflectionProbe";
-import { Material, MeshStandardMaterial, ShaderMaterial, Texture, VideoTexture } from "three";
+import { NoColorSpace } from "three";
 
 export class CombinedVideo extends Behaviour {
 
@@ -12,86 +11,11 @@ export class CombinedVideo extends Behaviour {
 
     start(): void {
 
-        if(this.player == null || this.renderer == null)
+        const vidTexture = this.player?.videoTexture;
+        if(vidTexture == null || this.renderer == null)
             return;
             
-        this.renderer.sharedMaterial["_VideoTex"] = this.player.videoTexture;
+        vidTexture.colorSpace = NoColorSpace;
+        this.renderer.sharedMaterial["_VideoTex"] = vidTexture;
     }
-
-/*
-    @serializable(VideoPlayer)
-    colorPlayer?: VideoPlayer;
-
-    @serializable(Renderer)
-    colorRenderer?: Renderer;
-
-     @serializable(VideoPlayer)
-    maskPlayer?: VideoPlayer;
-
-    @serializable(Renderer)
-    maskRenderer?: Renderer;
-
-    @serializable(Renderer)
-    combinedRenderer?: Renderer;
-
-    @serializable(VideoPlayer)
-    referencePlayer?: VideoPlayer;
-
-    private readyVideos: number = 0;
-    private readonly expectedVideos = 3;
-
-    startVideos(): void {
-        this.startVideo(this.colorPlayer!);
-        this.startVideo(this.maskPlayer!);
-        this.startVideo(this.referencePlayer!);
-    }
-
-    // play video, hook to the OnCanPlay event and pause the video if it's not the last one
-    startVideo(player: VideoPlayer) {
-
-        if(player === null)
-            return;
-
-        player.play();
-
-        if(player.videoElement == null)
-            console.error(`Can't play video at ${this.gameObject.name} (Video element is null)`);
-        else
-            player.videoElement.oncanplay = () => this.handleVideoReady(player);
-    }
-
-    handleVideoReady(player: VideoPlayer) {
-
-        // Increase the global ready count and pause if first, play if last
-        this.readyVideos++;
-        if(this.readyVideos < this.expectedVideos)
-            player.pause();
-        else if (this.readyVideos === this.expectedVideos)
-            this.setupCombinedVideo();
-    }
-
-    setupCombinedVideo() {
-
-        this.setupPlayer(this.maskPlayer!);
-        this.setupPlayer(this.colorPlayer!);
-        this.setupPlayer(this.referencePlayer!);
-
-        if(this.combinedRenderer == null || this.colorRenderer == null || this.maskRenderer == null)
-            return;
-
-        // fetch video textures and set them to combined video material
-        this.combinedRenderer.sharedMaterial["_ColorMap"] = this.colorRenderer.sharedMaterial["map"];
-        this.combinedRenderer.sharedMaterial["_MaskMap"]  = this.maskRenderer.sharedMaterial["map"];
-    }
-
-    setupPlayer(player: VideoPlayer) {
-
-        if(player == null)
-            return;
-            
-        player.playInBackground = true;
-        player.currentTime = 0;
-        player.play();
-    } */
-
 }
--- package/Runtime/VideoPlayer/Scripts/Needle.Sample.VideoPlayer~/codegen/register_types.ts
+++ package/Runtime/VideoPlayer/Scripts/Needle.Sample.VideoPlayer~/codegen/register_types.ts
@@ -1,7 +1,7 @@
 ﻿import { TypeStore } from "@needle-tools/engine"
 
 // Import types
-import { CombinedVideo } from "../CombinedVideo";
+import { CombinedVideo } from "../CombinedVideo.js";
 
 // Register types
 TypeStore.add("CombinedVideo", CombinedVideo);
