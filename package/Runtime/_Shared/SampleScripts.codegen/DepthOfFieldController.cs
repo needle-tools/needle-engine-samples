b0e3f09aa15da5f7808234e2b46855dee35af97c
Krystof <kipash612@gmail.com>
2023-08-28 21:06:06 +0200

    add DepthOfFieldController

---
+++ package/Runtime/_Shared/SampleScripts.codegen/DepthOfFieldController.cs
@@ -0,0 +1,18 @@
+// NEEDLE_CODEGEN_START
+// auto generated code - do not edit directly
+
+#pragma warning disable
+
+namespace Needle.Typescript.GeneratedComponents
+{
+	public partial class DepthOfFieldController : UnityEngine.MonoBehaviour
+	{
+		public float @apertureMultiplier = 2f;
+		public float @focalLength = 0.001f;
+		public float @focusDistanceCutoff = 25f;
+		public void OnEnable(){}
+		public void onBeforeRender(){}
+	}
+}
+
+// NEEDLE_CODEGEN_END
---
+++ package/Runtime/_Shared/SampleScripts~/DepthOfFieldController.ts
@@ -0,0 +1,46 @@
+import { Behaviour, DepthOfField, GameObject, Mathf, OrbitControls, Volume, serializable } from "@needle-tools/engine";
+import { getWorldPosition } from "@needle-tools/engine/src/engine/engine_three_utils";
+
+// Documentation → https://docs.needle.tools/scripting
+
+export class DepthOfFieldController extends Behaviour {
+    
+    @serializable()
+    apertureMultiplier: number = 2;
+
+    @serializable()
+    focalLength: number = 0.001;
+
+    @serializable()
+    focusDistanceCutoff: number = 25;
+
+    private volume : Volume | null = null;
+    private orbit?: OrbitControls;
+    private dofEffect: DepthOfField | undefined = undefined;
+
+    onEnable(): void {
+        this.volume = GameObject.findObjectOfType(Volume);
+        this.dofEffect = this.volume?.sharedProfile?.components?.find(c => c.typeName === "DepthOfField") as DepthOfField;
+        this.orbit = GameObject.findObjectOfType(OrbitControls)!;
+    }
+
+    onBeforeRender() {
+        const distance = this.orbit?.distanceToTarget(getWorldPosition(this.orbit.gameObject))!;
+        if (this.dofEffect) {
+            this.dofEffect.focusDistance.value = distance;
+            this.dofEffect.focalLength.value = this.focalLength;
+            let invLerp = Mathf.inverseLerp(0, this.focusDistanceCutoff, distance);
+            invLerp = Mathf.clamp01(invLerp);
+            this.dofEffect.aperture.value = Mathf.lerp(this.apertureMultiplier, 32, invLerp);
+            const threshold = this.focusDistanceCutoff;
+            if (distance > threshold && this.dofEffect.active) {
+                this.dofEffect.active = false;
+                //this.volume?.scheduleRecreate();
+            }
+            else if (distance < threshold && !this.dofEffect.active) {
+                this.dofEffect.active = true;
+                //this.volume?.scheduleRecreate();
+            }            
+        }
+    }
+}
