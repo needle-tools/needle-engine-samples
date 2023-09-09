039faf690468f3867207c0b5717354af09cacb72
Marcel Wiessler <marcel@gaisterhand.de>
2023-09-09 19:13:29 +0200

    Fix typescript complaining about property not existing on GameObject

--- package/Runtime/Scrollytelling/Needle.Samples.Scrollytelling~/MouseRotation.ts
+++ package/Runtime/Scrollytelling/Needle.Samples.Scrollytelling~/MouseRotation.ts
@@ -1,19 +1,19 @@
 import { Behaviour } from "@needle-tools/engine";
 import { Mathf } from "@needle-tools/engine";
-
+import { Object3D } from "three";
 
 
 export class MouseRotation extends Behaviour {
 
-    private targetRotation : number = 0;
+    private targetRotation: number = 0;
 
-    maxRotation : number = .1;
+    maxRotation: number = .1;
 
     update() {
         const rc = this.context.input.getPointerPositionRC(0);
-        if(!rc) return;
+        if (!rc) return;
         this.targetRotation = rc.x * this.maxRotation;
-        this.gameObject.rotation.y = Mathf.lerp(this.gameObject.rotation.y, this.targetRotation, this.context.time.deltaTime);
+        (this.gameObject as Object3D).rotation.y = Mathf.lerp((this.gameObject as Object3D).rotation.y, this.targetRotation, this.context.time.deltaTime);
     }
 
 }
