33bcb83e687021494657197e8a6ac897d1713970
Marcel Wiessler <marcel@gaisterhand.de>
2023-09-11 09:45:51 +0200

    Lightmap baker: show scene notification

--- package/Runtime/Lightmaps/Editor/LightmapBakerBuildCheck.cs
+++ package/Runtime/Lightmaps/Editor/LightmapBakerBuildCheck.cs
@@ -1,7 +1,10 @@
+using System;
 using System.Threading.Tasks;
 using Needle.Engine.Core;
 using Needle.Engine.Interfaces;
+using UnityEditor;
 using UnityEngine;
+using Object = UnityEngine.Object;
 
 namespace Needle.MultiLightmaps
 {
@@ -14,16 +17,30 @@ namespace Needle.MultiLightmaps
                 var baker = Object.FindObjectOfType<LightmapBaker>();
                 if (baker)
                 {
-                    // ensure that we have a lightmap baked for all configurations
-                    for (var index = 0; index < baker.Configurations.Count; index++)
+                    try
                     {
-                        var conf = baker.Configurations[index];
-                        if (!conf.BakedLightmap)
+                        // ensure that we have a lightmap baked for all configurations
+                        for (var index = 0; index < baker.Configurations.Count; index++)
                         {
-                            Debug.Log($"Baking lightmap for configuration {index} \"{conf.Name}\" before exporting...");
-                            var res = await baker.BakeAsync(index);
-                            if(!res) Debug.LogError($"Baking lightmap configuration {index} failed. Please check the console for errors", baker);
+                            var conf = baker.Configurations[index];
+                            if (!conf.BakedLightmap)
+                            {
+                                Debug.Log(
+                                    $"Baking lightmap for configuration {index} \"{conf.Name}\" before exporting...",
+                                    baker);
+                                SceneView.lastActiveSceneView?.ShowNotification(
+                                    new GUIContent("Baking lightmap " + conf.Name + "..."));
+                                var res = await baker.BakeAsync(index);
+                                if (!res)
+                                    Debug.LogError(
+                                        $"Baking lightmap configuration {index} failed. Please check the console for errors",
+                                        baker);
+                            }
                         }
+                    }
+                    catch (Exception ex)
+                    {
+                        Debug.LogError("Lightmap baking failed with an exception: " + ex, baker);
                     }
                 }
             }
