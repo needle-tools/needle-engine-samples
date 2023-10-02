a84365aeaa178691466f84096df900bf86bffedc
hybridherbst <felix.herbst@gmail.com>
2023-10-02 19:35:38 +0200

    replaced with fixed saxophone model

--- package/Runtime/SpatialAudio/Needle.SpatialAudio~/codegen/register_types.ts
+++ package/Runtime/SpatialAudio/Needle.SpatialAudio~/codegen/register_types.ts
@@ -1,8 +1,8 @@
 ﻿import { TypeStore } from "@needle-tools/engine"
 
 // Import types
-import { OrbitControlsView } from "../OrbitControlsView";
-import { SpatialAudioUI } from "../SpatialAudioUI";
+import { OrbitControlsView } from "../OrbitControlsView.js";
+import { SpatialAudioUI } from "../SpatialAudioUI.js";
 
 // Register types
 TypeStore.add("OrbitControlsView", OrbitControlsView);
