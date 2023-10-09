5c79f7f58858e15b27a6bd5223cbfaa9f7cbca68
hybridherbst <felix.herbst@gmail.com>
2023-10-09 08:08:49 +0200

    add gaussian splatting sample
    updated to newer version
    add comments, add XR
    test for streaming the splat data instead of waiting for all of it
    move over latest changes from quadjr/aframe-gaussian-splatting
    more tests including animation and multiple splats in one scene
    update with latest renderer version

---
+++ package/Runtime/GaussianSplats/Needle.GaussianSplats.codegen/SplatRenderer.cs
@@ -0,0 +1,17 @@
+// NEEDLE_CODEGEN_START
+// auto generated code - do not edit directly
+
+#pragma warning disable
+
+namespace Needle.Typescript.GeneratedComponents
+{
+	public partial class SplatRenderer : UnityEngine.MonoBehaviour
+	{
+		public string @path = "";
+		public UnityEngine.Vector4 @cutout;
+		public void start(){}
+		public void update(){}
+	}
+}
+
+// NEEDLE_CODEGEN_END
