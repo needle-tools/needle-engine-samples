9dd9e8b550677d0cfac3b2d85f60482957015f10
Marcel Wiessler <marcel@gaisterhand.de>
2023-11-06 14:07:39 +0100

    Add "NeedsURP" component and fail export if the scene doesnt have a URP asset assigned

---
+++ package/Runtime/Stencil Portals/Scripts/Editor/NeedsURPCheck.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using Needle.Engine.Core;
+using UnityEditor;
+using UnityEngine.Rendering;
+using Object = UnityEngine.Object;
+
+namespace Needle
+{
+	internal static class NeedsURPCheck
+	{
+		[InitializeOnLoadMethod]
+		static void Init()
+		{
+			Builder.BuildStarting += () =>
+			{
+				var needsUrpComponent = Object.FindObjectOfType<NeedsURP>();
+				if (needsUrpComponent)
+				{
+					if (!GraphicsSettings.currentRenderPipeline ||
+					    GraphicsSettings.currentRenderPipeline.GetType().Name.Contains("Universal", StringComparison.InvariantCultureIgnoreCase) == false)
+					{
+						EditorGUIUtility.PingObject(needsUrpComponent);
+						throw new Exception(
+							"This scene requires the Universal Render Pipeline. Please switch to URP in Project Settings → Graphics\nIf you think this is a mistake you can remove the \"NeedsURP\" component from the scene");
+					}
+				}
+			};
+		}
+	}
+}
---
+++ package/Runtime/Stencil Portals/Scripts/NeedsURP.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using UnityEngine;
+
+namespace Needle
+{
+	public class NeedsURP : MonoBehaviour
+	{
+	}
+}
