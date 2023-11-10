4defd41d4fb8cba28aa6e81e244c91483d23115d
Krystof <kipash612@gmail.com>
2023-11-10 17:07:13 +0100

    add physics test scene

--- package/Runtime/Character Framework/Scripts/Characters.codegen/Lift.cs
+++ package/Runtime/Character Framework/Scripts/Characters.codegen/Lift.cs
@@ -8,6 +8,7 @@ namespace Needle.Typescript.GeneratedComponents
 	public partial class Lift : UnityEngine.MonoBehaviour
 	{
 		public float @speed = 1f;
+		public float @cycleOffset = 0f;
 		public UnityEngine.Vector3 @offset = new UnityEngine.Vector3(0f, 1f, 0f);
 		public void awake(){}
 		public void update(){}
