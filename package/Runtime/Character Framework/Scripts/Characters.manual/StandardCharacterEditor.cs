096fe989ea6797e8026b435771ecb5296ca137c9
Krystof <kipash612@gmail.com>
2023-11-13 17:04:51 +0100

    remove unnecessary style adjustments ; add descriptions and add tooltip for "added" label

--- package/Runtime/Character Framework/Scripts/Characters.manual/StandardCharacterEditor.cs
+++ package/Runtime/Character Framework/Scripts/Characters.manual/StandardCharacterEditor.cs
@@ -1,7 +1,5 @@
-using System.Reflection;
+using System;
 using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.UIElements;
 
 namespace Needle.Typescript.GeneratedComponents
 {
@@ -9,24 +7,24 @@ namespace Needle.Typescript.GeneratedComponents
     [UnityEditor.CustomEditor(typeof(StandardCharacter))]
     public class StandardCharacterEditor : UnityEditor.Editor
     {
-        protected System.Type[] mandatoryModules = new System.Type[]
+        protected (Type type, string description)[] mandatoryModules = new (Type, string)[]
         {
-            typeof(PersonCamera),
-            typeof(DesktopCharacterInput),
-            typeof(MobileCharacterInput),
-            typeof(CharacterPhysics)
+            (typeof(PersonCamera), "First and third person camera."),
+            (typeof(DesktopCharacterInput), "Keyboard input for movement, sprinting and jumping."),
+            (typeof(MobileCharacterInput), "Touch screen input for movement, sprinting and jumping."),
+            (typeof(CharacterPhysics), "Physics of an character that can walk, jump and stick to platforms.")
         };
 
-        protected System.Type[] optionalModules = new System.Type[]
+        protected (Type type, string description)[] optionalModules = new (Type, string)[]
         {
-            typeof(CommonAvatar),
-            typeof(CommonCharacterAnimations),
-            typeof(CommonCharacterAudio)
+            (typeof(CommonAvatar), "Base for an Avatar that needs to react to first or third person adjustments."),
+            (typeof(CommonCharacterAnimations), "Plays movement animations."),
+            (typeof(CommonCharacterAudio), "Plays footstep, landing and jumping sounds.")
         };
 
         const string foldoutPrefsKey = "Character_Modules_Foldout";
         private bool foldout = false;
-        private GUIStyle addButton;
+        //private GUIStyle addButton;
         private GUIStyle addedButton;
         private GUIStyle headerStyle;
         private GUIStyle foldoutStyle;
@@ -34,29 +32,25 @@ namespace Needle.Typescript.GeneratedComponents
 
         private void OnEnable()
         {
-            foldout = UnityEditor.EditorPrefs.GetBool(foldoutPrefsKey, true);
+            foldout = UnityEditor.EditorPrefs.GetBool(foldoutPrefsKey, false);
         }
+
+        private GUIContent addedContent = new GUIContent("Added", "The module is already added on this object or its children!");
 
         public override void OnInspectorGUI()
         {
             if(addedButton == null)
             {
-                addButton = new GUIStyle(GUI.skin.button);
-                addButton.alignment = TextAnchor.MiddleLeft;
-
                 addedButton = new GUIStyle(GUI.skin.label);
                 addedButton.normal.textColor = new Color(0.5f, 0.5f, 0.5f);
 
-                headerStyle = new GUIStyle(GUI.skin.label);
-                headerStyle.fontStyle = FontStyle.Bold;
-                headerStyle.fontSize += 2;
+                headerStyle = new GUIStyle(UnityEditor.EditorStyles.boldLabel);
                 headerStyle.wordWrap = true;
 
                 wordWrapStyle = UnityEditor.EditorStyles.wordWrappedLabel;
 
                 foldoutStyle = new GUIStyle(UnityEditor.EditorStyles.foldout);
                 foldoutStyle.fontStyle = FontStyle.Bold;
-                foldoutStyle.fontSize += 4;
             }
 
             base.OnInspectorGUI();
@@ -85,37 +79,38 @@ namespace Needle.Typescript.GeneratedComponents
             UnityEditor.EditorGUI.EndFoldoutHeaderGroup();
         }
 
-        void ListModules(System.Type[] types)
+        void ListModules((Type type, string description)[] types)
         {
             var obj = (target as Character).gameObject;
             if (obj == null) return;
 
-            foreach (var moduleType in types)
+            foreach (var typeInfo in types)
             {
-                var module = obj?.GetComponentInChildren(moduleType);
-                //UnityEditor.EditorGUI.BeginDisabledGroup(module != null);
+                var module = obj?.GetComponentInChildren(typeInfo.type);
 
                 GUILayout.BeginHorizontal();
-                using (new GUILayout.HorizontalScope(GUILayout.MaxWidth(60)))
+                using (new GUILayout.HorizontalScope(GUILayout.MaxWidth(50)))
                 {
                     if (module != null)
                     {
-                        if (GUILayout.Button("Added", addedButton))
+                        if (GUILayout.Button(addedContent, addedButton))
                             UnityEditor.EditorGUIUtility.PingObject(obj);
                     }
                     else 
                     {
-                        if (GUILayout.Button("Add", addButton))
-                            obj.AddComponent(moduleType);
+                        if (GUILayout.Button("Add"))
+                            obj.AddComponent(typeInfo.type);
                     }
 
                     GUILayout.FlexibleSpace();
                 }
 
-                GUILayout.Space(10);
+                GUILayout.Space(5);
 
-                GUILayout.Label(moduleType.Name, GUILayout.Width(150));
-                        
+                GUILayout.Label(typeInfo.type.Name, GUILayout.Width(200));
+                GUILayout.Space(10);
+                GUILayout.Label(typeInfo.description, wordWrapStyle);
+
                 GUILayout.FlexibleSpace();
 
                 GUILayout.EndHorizontal();
