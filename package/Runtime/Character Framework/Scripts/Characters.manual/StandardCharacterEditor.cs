db79891d2714f5a7157a792b3d8a341db6fd14db
Krystof <kipash612@gmail.com>
2023-11-13 16:30:16 +0100

    polish StandardCharacter editor style

--- package/Runtime/Character Framework/Scripts/Characters.manual/StandardCharacterEditor.cs
+++ package/Runtime/Character Framework/Scripts/Characters.manual/StandardCharacterEditor.cs
@@ -1,7 +1,7 @@
 using System.Reflection;
-using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 namespace Needle.Typescript.GeneratedComponents
 {
@@ -26,9 +26,11 @@ namespace Needle.Typescript.GeneratedComponents
 
         const string foldoutPrefsKey = "Character_Modules_Foldout";
         private bool foldout = false;
-        private GUIStyle buttonStyle;
+        private GUIStyle addButton;
+        private GUIStyle addedButton;
         private GUIStyle headerStyle;
-        private GUIStyle mainHeaderStyle;
+        private GUIStyle foldoutStyle;
+        private GUIStyle wordWrapStyle;
 
         private void OnEnable()
         {
@@ -37,83 +39,95 @@ namespace Needle.Typescript.GeneratedComponents
 
         public override void OnInspectorGUI()
         {
-            if(buttonStyle == null)
+            if(addedButton == null)
             {
-                buttonStyle = new GUIStyle(GUI.skin.button);
-                buttonStyle.alignment = TextAnchor.MiddleLeft;
-            }
+                addButton = new GUIStyle(GUI.skin.button);
+                addButton.alignment = TextAnchor.MiddleLeft;
 
-            if (headerStyle == null)
-            {
+                addedButton = new GUIStyle(GUI.skin.label);
+                addedButton.normal.textColor = new Color(0.5f, 0.5f, 0.5f);
+
                 headerStyle = new GUIStyle(GUI.skin.label);
-                headerStyle.fontSize += 8;
+                headerStyle.fontStyle = FontStyle.Bold;
+                headerStyle.fontSize += 2;
+                headerStyle.wordWrap = true;
 
-                mainHeaderStyle = new GUIStyle(UnityEditor.EditorStyles.foldout);
-                mainHeaderStyle.fontSize += 8;
-                mainHeaderStyle.alignment = TextAnchor.MiddleCenter;
+                wordWrapStyle = UnityEditor.EditorStyles.wordWrappedLabel;
+
+                foldoutStyle = new GUIStyle(UnityEditor.EditorStyles.foldout);
+                foldoutStyle.fontStyle = FontStyle.Bold;
+                foldoutStyle.fontSize += 4;
             }
 
             base.OnInspectorGUI();
 
-            DrawLine();
+            GUILayout.Space(10);
 
-            using (new GUILayout.HorizontalScope())
-            {
-                GUILayout.FlexibleSpace();
-                foldout = UnityEditor.EditorGUILayout.BeginFoldoutHeaderGroup(foldout, "Modules", mainHeaderStyle);
-                GUILayout.FlexibleSpace();
+            foldout = UnityEditor.EditorGUILayout.BeginFoldoutHeaderGroup(foldout, "Modules", style: foldoutStyle);
 
-            }
             UnityEditor.EditorPrefs.SetBool(foldoutPrefsKey, foldout);
             if (foldout)
             {
-                int indent = 5;
-                UnityEditor.EditorGUI.indentLevel += indent;
-
+                DrawLine(height: 1);
                 if (mandatoryModules.Length > 0)
                 {
-                    GUILayout.Label("Mandatory modules", headerStyle);
-                    GUILayout.Label("Are added on start with default values. Add them yourself to modify the default settings");
+                    GUILayout.Label("Default modules", headerStyle);
+                    GUILayout.Label("Default modules are added on start with default values. Add them to override the default settings.", wordWrapStyle);
                     ListModules(mandatoryModules);
 
                     GUILayout.Space(20);
-
-                    GUILayout.Label("Optional modules", headerStyle);
-                    GUILayout.Label("Add them and configure them in order to make them work");
-                    ListModules(optionalModules);
                 }
 
-                UnityEditor.EditorGUI.indentLevel -= indent;
+                GUILayout.Label("Optional modules", headerStyle);
+                GUILayout.Label("Optional modules implement single features and are not essential for the character to work.", wordWrapStyle);
+                ListModules(optionalModules);
             }
             UnityEditor.EditorGUI.EndFoldoutHeaderGroup();
         }
 
         void ListModules(System.Type[] types)
         {
+            var obj = (target as Character).gameObject;
+            if (obj == null) return;
+
             foreach (var moduleType in types)
             {
-                var obj = (target as Character)?.gameObject;
                 var module = obj?.GetComponentInChildren(moduleType);
-                using (new UnityEditor.EditorGUI.DisabledGroupScope(module != null))
+                //UnityEditor.EditorGUI.BeginDisabledGroup(module != null);
+
+                GUILayout.BeginHorizontal();
+                using (new GUILayout.HorizontalScope(GUILayout.MaxWidth(60)))
                 {
-                    using (new GUILayout.HorizontalScope())
+                    if (module != null)
                     {
-                        if (GUILayout.Button(moduleType.Name, buttonStyle) && obj)
-                        {
-                            obj.AddComponent(moduleType);
-                        }
-                        GUILayout.FlexibleSpace();
+                        if (GUILayout.Button("Added", addedButton))
+                            UnityEditor.EditorGUIUtility.PingObject(obj);
                     }
+                    else 
+                    {
+                        if (GUILayout.Button("Add", addButton))
+                            obj.AddComponent(moduleType);
+                    }
+
+                    GUILayout.FlexibleSpace();
                 }
+
+                GUILayout.Space(10);
+
+                GUILayout.Label(moduleType.Name, GUILayout.Width(150));
+                        
+                GUILayout.FlexibleSpace();
+
+                GUILayout.EndHorizontal();
             }
         }
 
         void DrawLine(int height = 1, int spacing = 5, Color? color = null)
         {
-            GUILayout.Space(spacing + 4);
-            Rect rect = EditorGUILayout.GetControlRect(false, height);
+            GUILayout.Space(spacing);
+            Rect rect = UnityEditor.EditorGUILayout.GetControlRect(false, height);
             rect.height = height;
-            EditorGUI.DrawRect(rect, color ?? new Color(0.5f, 0.5f, 0.5f, .4f));
+            UnityEditor.EditorGUI.DrawRect(rect, color ?? new Color(0.5f, 0.5f, 0.5f, .4f));
             GUILayout.Space(spacing);
         }
     }
