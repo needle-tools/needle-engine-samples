d7f178125ea48457ecf2df5b88069884420eead8
Krystof <kipash612@gmail.com>
2024-01-03 17:00:07 +0100

    add JumpPad and Lift from core

---
+++ package/Runtime/Character Framework/Scripts/Needle.CharacterSamples.codegen/Lift.cs
@@ -0,0 +1,18 @@
+// NEEDLE_CODEGEN_START
+// auto generated code - do not edit directly
+
+#pragma warning disable
+
+namespace Needle.Typescript.GeneratedComponents
+{
+	public partial class Lift : UnityEngine.MonoBehaviour
+	{
+		public float @speed = 1f;
+		public float @cycleOffset = 0f;
+		public UnityEngine.Vector3 @offset = new UnityEngine.Vector3(0f, 1f, 0f);
+		public void awake(){}
+		public void update(){}
+	}
+}
+
+// NEEDLE_CODEGEN_END
