bee91cfb8bce2a78a7f7e31ed6937c68ed8f3157
Krystof <kipash612@gmail.com>
2024-01-04 15:58:38 +0100

    add DoubleJump character

---
+++ package/Runtime/Character Framework/Scripts/Needle.CharacterSamples.codegen/DoubleJumpCharacter.cs
@@ -0,0 +1,15 @@
+// NEEDLE_CODEGEN_START
+// auto generated code - do not edit directly
+
+#pragma warning disable
+
+namespace Needle.Typescript.GeneratedComponents
+{
+	public partial class DoubleJumpCharacter : Needle.Engine.Components.Experimental.StandardCharacter
+	{
+		public float @maxJumps = 2f;
+		public void initialize(bool @findModules){}
+	}
+}
+
+// NEEDLE_CODEGEN_END
