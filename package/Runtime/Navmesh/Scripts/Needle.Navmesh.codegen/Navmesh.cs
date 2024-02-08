a647faebbc0cb223da5f0fcb9f7d51cd81c79250
Krystof <kipash612@gmail.com>
2024-02-08 19:38:16 +0100

    adjust bake trigger logic

--- package/Runtime/Navmesh/Scripts/Needle.Navmesh.codegen/Navmesh.cs
+++ package/Runtime/Navmesh/Scripts/Needle.Navmesh.codegen/Navmesh.cs
@@ -3,6 +3,8 @@
 
 #pragma warning disable
 
+using UnityEditor;
+
 namespace Needle.Typescript.GeneratedComponents
 {
 	public partial class Navmesh : UnityEngine.MonoBehaviour
@@ -22,24 +24,55 @@ namespace Needle.Typescript.GeneratedComponents
 
         UnityEngine.Mesh GetMesh()
         {
+            var wasSceneDirty = gameObject.scene.isDirty;
+
+            UnityEngine.AI.NavMeshTriangulation oldMesh = UnityEngine.AI.NavMesh.CalculateTriangulation();
+
+            var anyData = oldMesh.vertices.Length > 1;
+
 #if UNITY_EDITOR
-            if (bakeNavmeshOnExport)
+            if (bakeNavmeshOnExport && (wasSceneDirty || anyData))
                 UnityEditor.AI.NavMeshBuilder.BuildNavMesh();
 #endif
-            UnityEngine.AI.NavMeshTriangulation triangulatedNavMesh = UnityEngine.AI.NavMesh.CalculateTriangulation();
+            UnityEngine.AI.NavMeshTriangulation newMesh = UnityEngine.AI.NavMesh.CalculateTriangulation();
+
+            //bool hasChanged = false;
+            //if (oldMesh.vertices.Length != newMesh.vertices.Length)
+            //    hasChanged = true;
+            //else
+            //{
+            //    for (int i = 0; i < oldMesh.vertices.Length; i++)
+            //    {
+            //        var a = oldMesh.vertices[i];
+            //        var b = newMesh.vertices[i];
+
+            //        if (!VectorsApproximatelyEquel(a, b))
+            //        {
+            //            hasChanged = true;
+            //            break;
+            //        }
+            //    }
+            //}
 
             var mesh = new UnityEngine.Mesh();
             mesh.name = "ExportedNavMesh";
-            for (int i = 0; i < triangulatedNavMesh.vertices.Length; i++)
+            for (int i = 0; i < newMesh.vertices.Length; i++)
             {
-                var v3 = triangulatedNavMesh.vertices[i];
+                var v3 = newMesh.vertices[i];
                 //v3.x *= -1;
-                triangulatedNavMesh.vertices[i] = v3;
+                newMesh.vertices[i] = v3;
             }
-            mesh.vertices = triangulatedNavMesh.vertices;
-            mesh.triangles = triangulatedNavMesh.indices;//.Reverse().ToArray();
+            mesh.vertices = newMesh.vertices;
+            mesh.triangles = newMesh.indices;//.Reverse().ToArray();
 
             return mesh;
         }
+
+        //bool VectorsApproximatelyEquel(UnityEngine.Vector3 a, UnityEngine.Vector3 b)
+        //{
+        //    return UnityEngine.Mathf.Approximately(a.x, b.x) &&
+        //           UnityEngine.Mathf.Approximately(a.y, b.y) &&
+        //           UnityEngine.Mathf.Approximately(a.z, b.z);
+        //}
     }
 }
