721818a9a310b8ec70d5ceae9632872a0616d109
Krystof <kipash612@gmail.com>
2024-02-12 20:04:48 +0100

    test rapier's DynamicRayCastVehicleController

---
+++ package/Runtime/CarPhysics/Scripts/Needle.Samples.CarPhysics.codegen/CarWheel.cs
@@ -0,0 +1,18 @@
+// NEEDLE_CODEGEN_START
+// auto generated code - do not edit directly
+
+#pragma warning disable
+
+namespace Needle.Typescript.GeneratedComponents
+{
+	public partial class CarWheel : UnityEngine.MonoBehaviour
+	{
+		public bool @isFront = false;
+		public UnityEngine.GameObject @axelRef;
+		public float @radius = 1f;
+		public float @suspension = 1f;
+		public float @maxSusspensionForce = 20f;
+	}
+}
+
+// NEEDLE_CODEGEN_END
