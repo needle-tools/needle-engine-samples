7211223d21eb106d6b7a3fca92ca63ea69fe1812
Marcel Wiessler <marcel@gaisterhand.de>
2024-02-20 22:09:39 +0800

    deprecate ClickToOpenWebsite

--- package/Runtime/MiscScripting/ScriptingSamples.codegen/ClickToOpenWebsite.cs
+++ package/Runtime/MiscScripting/ScriptingSamples.codegen/ClickToOpenWebsite.cs
@@ -8,14 +8,8 @@ namespace Needle.Typescript.GeneratedComponents
 	public partial class ClickToOpenWebsite : UnityEngine.MonoBehaviour
 	{
 		public string @url;
-		public bool @keepQuery = false;
-		public bool @keepHash = false;
 		public bool @openNewTab = false;
 		public void awake(){}
-		public void onClick(){}
-		public void onPointerClick(UnityEngine.EventSystems.PointerEventData @args){}
-		public void onPointerEnter(){}
-		public void onPointerExit(){}
 	}
 }
 
--- package/Runtime/MiscScripting/ScriptingSamples~/ClickToOpenWebsite.ts
+++ package/Runtime/MiscScripting/ScriptingSamples~/ClickToOpenWebsite.ts
@@ -1,57 +1,19 @@
-import { Behaviour } from "@needle-tools/engine";
-import { PointerEventData, IPointerEventHandler } from "@needle-tools/engine";
-import { WebXRController, ControllerEvents } from "@needle-tools/engine";
+import { Behaviour, OpenURL, serializable } from "@needle-tools/engine";
+import { IPointerEventHandler } from "@needle-tools/engine";
 
 export class ClickToOpenWebsite extends Behaviour implements IPointerEventHandler {
 
+    @serializable()
     url: string | null = null;
-    keepQuery: boolean = false;
-    keepHash: boolean = false;
+    @serializable()
     openNewTab: boolean = false;
 
     awake(): void {
-        WebXRController.addEventListener(ControllerEvents.SelectStart, (_controller, args: { selected: THREE.Object3D }) => {
-            if (args.selected === this.gameObject) {
-                this.onClick();
-            }
-        })
-    }
-
-    private lastClickTime: number = 0;
-
-    onClick() {
-        // hack/workaround until we support onclick for xr controllers
-        if (this.context.time.time - this.lastClickTime < .3) return;
-        this.lastClickTime = this.context.time.time;
-
-        if (!this.url || this.url.length <= 0) {
-            this.url = window.location.href;
-        }
-
         if (this.url) {
-            const targetUrl = new URL(this.url);
-            if (this.keepQuery) {
-                if (targetUrl.search) targetUrl.search += "&" + window.location.search.substring(1);
-                else targetUrl.search = window.location.search;
-            }
-            if (this.keepHash) {
-                if (targetUrl.hash) targetUrl.hash += "&" + window.location.hash.substring(1);
-                else targetUrl.hash = window.location.hash;
-            }
-            window.open(targetUrl, this.openNewTab ? "_blank" : "_self");
+            console.warn("ClickToOpenWebsite: URL is deprecated: use OpenURL instead.");
+            const openUrl = this.gameObject.addNewComponent(OpenURL)!;
+            openUrl.url = this.url;
+            openUrl.mode = this.openNewTab ? 0 : 1;
         }
-    }
-
-    onPointerClick(args: PointerEventData) {
-        this.onClick();
-        args.Use();
-    }
-
-    onPointerEnter() {
-        this.context.input.setCursorPointer();
-    }
-
-    onPointerExit() {
-        this.context.input.setCursorNormal();
     }
 }
