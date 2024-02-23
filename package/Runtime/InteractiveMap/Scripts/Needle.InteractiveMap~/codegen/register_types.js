c74002a521f3b7fce11a04720f0ef364437229fb
Krystof <kipash612@gmail.com>
2024-02-23 15:20:24 +0100

    web projects fixes ; other npmdefs

--- package/Runtime/InteractiveMap/Scripts/Needle.InteractiveMap~/codegen/register_types.js
+++
@@ -1,9 +0,0 @@
-﻿import { TypeStore } from "@needle-tools/engine"
-
-// Import types
-import { MapLocator } from "../MapLocator";
-import { DisplayMap } from "../MapView";
-
-// Register types
-TypeStore.add("MapLocator", MapLocator);
-TypeStore.add("DisplayMap", DisplayMap);
---
+++ package/Runtime/InteractiveMap/Scripts/Needle.InteractiveMap~/codegen/register_types.ts
@@ -0,0 +1,10 @@
+﻿/* eslint-disable */
+import { TypeStore } from "@needle-tools/engine"
+
+// Import types
+import { MapLocator } from "../MapLocator.js";
+import { DisplayMap } from "../MapView.js";
+
+// Register types
+TypeStore.add("MapLocator", MapLocator);
+TypeStore.add("DisplayMap", DisplayMap);
