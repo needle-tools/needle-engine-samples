992ad4307a4bf02918e224c0cb0d7a8af256f1d1
hybridherbst <felix.herbst@gmail.com>
2024-04-02 16:55:22 +0200

    simplify hotspot by making it single line
    related to https://linear.app/needle/issue/NE-4255

--- package/Runtime/Hotspots/Scripts/Editor/HotspotEditor.cs
+++ package/Runtime/Hotspots/Scripts/Editor/HotspotEditor.cs
@@ -9,13 +9,11 @@ namespace Needle.Typescript.GeneratedComponents
     [CustomEditor(typeof(Hotspot)), CanEditMultipleObjects]
     public class HotspotEditor : Editor
     {
-        // ---- Inspector -----
-
         SerializedProperty titleProperty;
         SerializedProperty contentProperty;
         SerializedProperty viewAngleProperty;
 
-        void OnEnable()
+        private void OnEnable()
         {
             titleProperty = serializedObject.FindProperty("titleText");
             contentProperty = serializedObject.FindProperty("contentText");
@@ -26,34 +24,17 @@ namespace Needle.Typescript.GeneratedComponents
         {
             serializedObject.Update();
 
-            var multi = serializedObject.isEditingMultipleObjects;
-
-            var titleUserVal = EditorGUILayout.TextField(multi ? "-" : titleProperty.stringValue);
-            if(!multi || titleUserVal != "-")
-                titleProperty.stringValue = titleUserVal;
-
-            var oldState = EditorStyles.textField.wordWrap;
-            EditorStyles.textField.wordWrap = true;
-            var contentUserVal = EditorGUILayout.TextArea(multi ? "-" : contentProperty.stringValue, GUILayout.Height(80));
-            EditorStyles.textField.wordWrap = oldState;
-
-            if (!multi || contentUserVal != "-")
-                contentProperty.stringValue = contentUserVal;
-
-            EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
-
+            EditorGUILayout.PropertyField(titleProperty);
+            EditorGUILayout.PropertyField(contentProperty);
             EditorGUILayout.Slider(viewAngleProperty, 0, 180);
 
             serializedObject.ApplyModifiedProperties();
         }
 
-        // ---- Scene -----
-
-        const float GizmosConeLength = 5;
-        const int GizmosConeSegments = 50;
-
-        Hotspot hotspot => target as Hotspot;
-        Transform transform => hotspot.transform;
+        private const float GizmosConeLength = 5;
+        private const int GizmosConeSegments = 50;
+        private Hotspot hotspot => target as Hotspot;
+        private Transform transform => hotspot.transform;
 
         private void OnSceneGUI()
         {
@@ -63,30 +44,33 @@ namespace Needle.Typescript.GeneratedComponents
             DrawCone(transform, Color.red, hotspot.viewAngle);
         }
 
-        void DrawCone(Transform transform, Color color, float angle, int segments = GizmosConeSegments) =>
+        private void DrawCone(Transform transform, Color color, float angle, int segments = GizmosConeSegments)
+        {
             DrawCone(transform.position, transform.forward, transform.up, color, angle, segments);
-        void DrawCone(Vector3 origin, Vector3 fwd, Vector3 up, Color color, float angle, int segments = GizmosConeSegments)
+        }
+        
+        private void DrawCone(Vector3 origin, Vector3 fwd, Vector3 up, Color color, float angle, int segments = GizmosConeSegments)
         {
             //Circle
             Handles.color = color;
-            Draw((a, b) => Handles.DrawLine(a, b),
-                 origin, fwd, up, angle, segments);
+            Draw((a, b) => Handles.DrawLine(a, b), origin, fwd, up, angle, segments);
 
             //Cone
             Handles.color = color * .4f;
-            Draw((a, b) => Handles.DrawLine(origin, a),
-                 origin, fwd, up, angle, segments);
+            Draw((a, b) => Handles.DrawLine(origin, a), origin, fwd, up, angle, segments);
         }
 
-        void Draw(Action<Vector3, Vector3> drawElement, Vector3 origin, Vector3 direction, Vector3 up, float angle, int segments)
+        private void Draw(Action<Vector3, Vector3> drawElement, Vector3 origin, Vector3 direction, Vector3 up, float angle, int segments)
         {
             angle = Mathf.Clamp(angle, 0, 180);
-
             var angledDir = Quaternion.AngleAxis(angle, up) * direction;
 
-            Vector3 CalculatePoint(int index) => origin + (Quaternion.AngleAxis((360f / segments) * index, direction) * angledDir) * GizmosConeLength;
+            Vector3 CalculatePoint(int index)
+            {
+                return origin + (Quaternion.AngleAxis((360f / segments) * index, direction) * angledDir) * GizmosConeLength;
+            }
 
-            for (int i = 0; i < segments; i++)
+            for (var i = 0; i < segments; i++)
                 drawElement(CalculatePoint(i), CalculatePoint(i + 1));
         }
     }
