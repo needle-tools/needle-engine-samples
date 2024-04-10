03f5b843b108893bbbc4aedb7919480dd6ff72f1
Krystof <kipash612@gmail.com>
2024-04-10 17:02:27 +0200

    add car model ; fix incorrect wheel direction setup ; expose suspension params

--- package/Runtime/CarPhysics/Scripts/Needle.Samples.CarPhysics.codegen/CarWheel.cs
+++ package/Runtime/CarPhysics/Scripts/Needle.Samples.CarPhysics.codegen/CarWheel.cs
@@ -8,10 +8,13 @@ namespace Needle.Typescript.GeneratedComponents
 	public partial class CarWheel : UnityEngine.MonoBehaviour
 	{
 		public bool @isFront = false;
-		public UnityEngine.GameObject @axelRef;
 		public float @radius = 1f;
-		public float @suspension = 1f;
-		public float @maxSusspensionForce = 20f;
+		public float @suspensionCompression = 0.82f;
+		public float @suspensionRelax = 0.88f;
+		public float @suspensionRestLength = 0.2f;
+		public float @suspensionStiff = 5.8f;
+		public float @suspensionForce = 6000f;
+		public float @suspensionTravel = 5f;
 	}
 }
 
--- package/Runtime/CarPhysics/Scripts/Needle.Samples.CarPhysics~/CarController.ts
+++ package/Runtime/CarPhysics/Scripts/Needle.Samples.CarPhysics~/CarController.ts
@@ -1,23 +1,27 @@
 import { DynamicRayCastVehicleController, World, Vector } from "@dimforge/rapier3d-compat";
-import { Behaviour, Collider, GameObject, Gizmos, Mathf, Rigidbody, getComponent, getTempQuaternion, getTempVector, serializable, setWorldPosition } from "@needle-tools/engine";
+import { Behaviour, Collider, GameObject, Gizmos, Mathf, Rigidbody, getComponent, getTempQuaternion, getTempVector, serializable, setWorldPosition, showBalloonMessage } from "@needle-tools/engine";
 import { Vector3 } from "three";
-import { xor } from "three/examples/jsm/nodes/Nodes.js";
 
 export class CarWheel extends Behaviour {
     @serializable()
     isFront: boolean = false;
 
-    @serializable(GameObject)
-    axelRef!: GameObject;
-
     @serializable()
     radius: number = 1;
 
+    // ---  Suspension --- 
     @serializable()
-    suspension: number = 1;
-
+    suspensionCompression: number = 0.82;
     @serializable()
-    maxSusspensionForce: number = 20;
+    suspensionRelax: number = 0.88;
+    @serializable()
+    suspensionRestLength: number = 0.2;
+    @serializable()
+    suspensionStiff: number = 5.8;
+    @serializable()
+    suspensionForce: number = 6000;
+    @serializable()
+    suspensionTravel: number = 5;
 
     //@nonSerialized
     localPosOnStart: Vector3 = new Vector3();
@@ -40,7 +44,7 @@ export class CarController extends Behaviour {
     private currAcc: number = 0;
     start(): void {
         let n_rb = this.gameObject.getComponent(Rigidbody);
-        n_rb ??= this.gameObject.addNewComponent(Rigidbody)!;
+        n_rb ??= this.gameObject.addComponent(Rigidbody);
         this.rigidbody = n_rb;
 
         const r_rb = this.context.physics.engine?.getBody(n_rb);
@@ -56,24 +60,37 @@ export class CarController extends Behaviour {
         this.wheels.forEach((wheel, i) => { 
             const wPos = wheel.worldPosition;
             const rPos = this.gameObject.worldToLocal(wPos);
-
-            const wDir = wheel.forward;
-            const rDir = this.gameObject.worldToLocal(wDir);
+            const rDir = getTempVector(0,-1,0);
 
             const wAxis = wheel.gameObject.worldRight;
             const rAxis = this.gameObject.worldToLocal(wAxis);
 
+            //showBalloonMessage(`${rDir.x.toFixed(1)}, ${rDir.y.toFixed(1)}, ${rDir.z.toFixed(1)}`);
             //Gizmos.DrawLine(this.worldPosition, wheel.worldPosition, "red", 5, false);
             //Gizmos.DrawSphere(wheel.worldPosition, 0.1, "red", 5, false);
             wheel.localPosOnStart.copy(rPos);
-            this.vehicle?.addWheel(rPos, rDir, rAxis, wheel.suspension, wheel.radius);
+            this.vehicle?.addWheel(rPos, rDir, rAxis, wheel.suspensionRestLength, wheel.radius);
 
-            // slip
-            //this.vehicle?.setWheelFrictionSlip(i, 0);
-            this.vehicle?.setWheelMaxSuspensionForce(i, wheel.maxSusspensionForce);
+            this.vehicle?.setWheelSuspensionCompression(i, wheel.suspensionCompression);
+            this.vehicle?.setWheelSuspensionRelaxation(i, wheel.suspensionRelax);
+            this.vehicle?.setWheelSuspensionStiffness(i, wheel.suspensionStiff);
+            this.vehicle?.setWheelMaxSuspensionForce(i, wheel.suspensionForce);
+            this.vehicle?.setWheelMaxSuspensionTravel(i, wheel.suspensionTravel);
+
+
+            /* const compression = this.vehicle?.wheelSuspensionCompression(i);
+            const length = this.vehicle?.wheelSuspensionLength(i);
+            const relax = this.vehicle?.wheelSuspensionRelaxation(i);
+            const rest = this.vehicle?.wheelSuspensionRestLength(i);
+            const stiff = this.vehicle?.wheelSuspensionStiffness(i);
+            const force = this.vehicle?.wheelMaxSuspensionForce(i);
+            const travel = this.vehicle?.wheelMaxSuspensionTravel(i); */
+            /* console.log(`Wheel ${i}: compression: ${compression}, length: ${length}, relax: ${relax}, rest: ${rest}, stiff: ${stiff}, force: ${force}, travel: ${travel}`); */
+
         });
     }
 
+    private hasResetAfterFirstUpdate = false;
     update() {
         this.handleInput();
 
@@ -84,6 +101,12 @@ export class CarController extends Behaviour {
         
         const dt = this.context.time.deltaTime;
         this.vehicle?.updateVehicle(dt);
+
+        if (!this.hasResetAfterFirstUpdate) {
+            this.hasResetAfterFirstUpdate = true;
+            this.rigidbody?.resetForces();
+            this.rigidbody?.resetVelocities();
+        }
 
         this.updateWheelVisual();
 
@@ -109,14 +132,13 @@ export class CarController extends Behaviour {
         this.wheels.forEach((wheel, i) => { 
             if (wheel.isFront) {
                 this.vehicle?.setWheelSteering(i, this.currSteer * this.maxSteer);
-                const debugPos = wheel.worldPosition;
+                /* const debugPos = wheel.worldPosition;
                 debugPos.y += 1;
-                Gizmos.DrawLabel(debugPos, (this.vehicle?.wheelSteering(i) ?? 0).toFixed(1), 0.1, 0, 0xffffff, 0x000000);
+                Gizmos.DrawLabel(debugPos, (this.vehicle?.wheelSteering(i) ?? 0).toFixed(1), 0.1, 0, 0xffffff, 0x000000); */
             }
-            else {
-                const s = this.currAcc * this.speed * (this.rigidbody?.mass ?? 1);
-                this.vehicle?.setWheelEngineForce(i, s);
-            }
+
+            const s = this.currAcc * this.speed * (this.rigidbody?.mass ?? 1);
+            this.vehicle?.setWheelEngineForce(i, s);
         });
     }
 
@@ -148,11 +170,11 @@ export class CarController extends Behaviour {
             if (wheelSlot) {
                 const vec = this.rapierVectorToThreeVector(wheelSlot);
                 this.gameObject.localToWorld(vec);
-                Gizmos.DrawLine(wheel.worldPosition, vec, "red", 0, false);
-                Gizmos.DrawSphere(vec, .1, "red", 0, false);
+                /* Gizmos.DrawLine(wheel.worldPosition, vec, "red", 0, false);
+                Gizmos.DrawSphere(vec, .1, "red", 0, false); */
 
-                const pos = getTempVector(wheel.worldPosition);
-                pos.y += wheel.radius;
+                /* const pos = getTempVector(wheel.worldPosition);
+                pos.y += wheel.radius; */
                 //Gizmos.DrawLabel(pos, );
             }
         });
