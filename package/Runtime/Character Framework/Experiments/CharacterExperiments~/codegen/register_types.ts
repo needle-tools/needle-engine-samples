83a98c595b1fb454467627195473eb4ccff4f75f
Krystof <kipash612@gmail.com>
2024-04-11 18:15:04 +0200

    fix missing prefabs ; remove old car experiment ; adjust animated character materials

--- package/Runtime/Character Framework/Experiments/CharacterExperiments~/codegen/register_types.ts
+++
@@ -1,7 +0,0 @@
-﻿import { TypeStore } from "@needle-tools/engine"
-
-// Import types
-import { Car } from "../Car.js";
-
-// Register types
-TypeStore.add("Car", Car);
--- package/Runtime/Character Framework/Scripts/Needle.CharacterSamples~/codegen/register_types.ts
+++ package/Runtime/Character Framework/Scripts/Needle.CharacterSamples~/codegen/register_types.ts
@@ -1,4 +1,5 @@
-﻿import { TypeStore } from "@needle-tools/engine"
+﻿/* eslint-disable */
+import { TypeStore } from "@needle-tools/engine"
 
 // Import types
 import { DoubleJumpCharacter } from "../Characters/DoubleJumpCharacter.js";
