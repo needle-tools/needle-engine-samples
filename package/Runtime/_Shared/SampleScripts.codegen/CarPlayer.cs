92771a8d03cf946c348cc63fa20b8e5f560ec70c
Krystof <kipash612@gmail.com>
2024-04-22 18:02:29 +0200

    migrate Car Physics npmdef to SampleScripts npmdef

---
+++ package/Runtime/_Shared/SampleScripts.codegen/CarPlayer.cs
@@ -0,0 +1,24 @@
+// NEEDLE_CODEGEN_START
+// auto generated code - do not edit directly
+
+#pragma warning disable
+
+namespace Needle.Typescript.GeneratedComponents
+{
+	public partial class CarPlayer : Needle.Engine.Components.Experimental.Player
+	{
+		public bool @autoSetupWheels = true;
+		public UnityEngine.GameObject[] @wheelModels = new UnityEngine.GameObject[]{ };
+		public UnityEngine.GameObject @wheelPrefab;
+		public void awake(){}
+		public void update(){}
+	}
+}
+
+// NEEDLE_CODEGEN_END
+
+namespace Needle.Typescript.GeneratedComponents
+{
+	public enum CarDrive { front, rear, all }
+	public enum CarAxle { front, rear }
+}
