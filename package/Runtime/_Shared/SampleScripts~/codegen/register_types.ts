6b742a5687e4e415268b1be2df35cb703a03d8b3
Krystof <kipash612@gmail.com>
2024-04-25 19:21:58 +0200

    add SyncedPlayableDirector

--- package/Runtime/_Shared/SampleScripts~/codegen/register_types.ts
+++ package/Runtime/_Shared/SampleScripts~/codegen/register_types.ts
@@ -46,6 +46,8 @@ import { Arrow } from "../Bow & Arrow/Arrow.js";
 import { ArrowShooting } from "../Bow & Arrow/ArrowShooting.js";
 import { BowArrowTarget } from "../Bow & Arrow/BowArrowTarget.js";
 import { BowTargetSpawner } from "../Bow & Arrow/BowTargetSpawner.js";
+import { SyncTimelineData } from "../Networking/SyncedPlayableDirector.js";
+import { SyncedPlayableDirector } from "../Networking/SyncedPlayableDirector.js";
 import { VariantInfo } from "../Variants/VariantInfo.js";
 import { VariantSwitcher } from "../Variants/VariantSwitcher.js";
 
@@ -94,5 +96,7 @@ TypeStore.add("Arrow", Arrow);
 TypeStore.add("ArrowShooting", ArrowShooting);
 TypeStore.add("BowArrowTarget", BowArrowTarget);
 TypeStore.add("BowTargetSpawner", BowTargetSpawner);
+TypeStore.add("SyncTimelineData", SyncTimelineData);
+TypeStore.add("SyncedPlayableDirector", SyncedPlayableDirector);
 TypeStore.add("VariantInfo", VariantInfo);
 TypeStore.add("VariantSwitcher", VariantSwitcher);
