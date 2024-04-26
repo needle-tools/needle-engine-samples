5c818dba126e12f443fa2694d099a9d766f1f9cf
Marcel Wiessler <marcel@gaisterhand.de>
2024-04-26 09:46:04 +0200

    simplify synced playable director

---
+++ package/Runtime/_Shared/SampleScripts.codegen/SyncedPlayableDirector.cs
@@ -0,0 +1,16 @@
+// NEEDLE_CODEGEN_START
+// auto generated code - do not edit directly
+
+#pragma warning disable
+
+namespace Needle.Typescript.GeneratedComponents
+{
+	public partial class SyncedPlayableDirector : UnityEngine.MonoBehaviour
+	{
+		public void awake(){}
+		public void OnEnable(){}
+		public void onAfterRender(){}
+	}
+}
+
+// NEEDLE_CODEGEN_END
