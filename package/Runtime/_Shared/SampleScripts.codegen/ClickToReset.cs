ad391237a415bb4b36cf82da9d69e80ffb3bd7f2
hybridherbst <felix.herbst@gmail.com>
2024-04-29 00:09:47 +0200

    add physics snowglobe

---
+++ package/Runtime/_Shared/SampleScripts.codegen/ClickToReset.cs
@@ -0,0 +1,17 @@
+// NEEDLE_CODEGEN_START
+// auto generated code - do not edit directly
+
+#pragma warning disable
+
+namespace Needle.Typescript.GeneratedComponents
+{
+	public partial class ClickToReset : UnityEngine.MonoBehaviour
+	{
+		public UnityEngine.GameObject @target;
+		public void start(){}
+		public void onPointerClick(UnityEngine.EventSystems.PointerEventData @_args){}
+		public void createBehaviours(object @ext, object @model, object @context){}
+	}
+}
+
+// NEEDLE_CODEGEN_END
