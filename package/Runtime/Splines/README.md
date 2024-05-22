a74aedc760e02975237aa642ade03489994b86b8
Krystof <kipash612@gmail.com>
2024-05-22 20:14:11 +0200

    [Splines] add README ; finalize scene ; finalize helper editor for SplineWalker
    https://linear.app/needle/issue/NE-4934/migrate-spline-support-into-a-sample-similar-to-navmesh

---
+++ package/Runtime/Splines/README.md
@@ -0,0 +1,4 @@
+# Splines
+Spline sample showcasing support for the [Unity Splines package](https://docs.unity3d.com/Packages/com.unity.splines@2.4/manual/index.html) which is available for Unity LTS 2022 and later.
+
+Bare in mind that the support is not complete yet.
--- package/Runtime/Splines/Scripts/Needle.Samples.Splines.codegen/SplineWalker.cs
+++ package/Runtime/Splines/Scripts/Needle.Samples.Splines.codegen/SplineWalker.cs
@@ -36,14 +36,25 @@ namespace Needle.Typescript.GeneratedComponents
 	[UnityEditor.CustomEditor(typeof(SplineWalker))]
     public class SplineWalkerEdiotr : UnityEditor.Editor
 	{
+		UnityEngine.GUIStyle labelSkin;
         public override void OnInspectorGUI()
         {
 #if SPLINES_PACAKGE
             base.OnInspectorGUI();
+#elif !UNITY_2022_1_OR_NEWER
+			if (labelSkin == null)
+			{
+                labelSkin = new UnityEngine.GUIStyle(UnityEditor.EditorStyles.boldLabel);
+				labelSkin.normal.textColor = new UnityEngine.Color(0.77f, 0.29f, 0.29f);
+            }
+
+			UnityEngine.GUILayout.Label("Splines pacakge requires Unity 2022.3+", labelSkin);
 #else
-			UnityEngine.GUILayout.Label("TODO: instructions to install pacakge + VERSION CHECK");
+			if (UnityEngine.GUILayout.Button("Install Splines package")) {
+				UnityEditor.PackageManager.UI.Window.Open("com.unity.splines");
+			}
 #endif
-		}
+        }
     }
 }
 
