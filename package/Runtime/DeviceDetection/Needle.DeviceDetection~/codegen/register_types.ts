79d42954b711769e6b0d6855e4e3d763301318b7
Krystof <kipash612@gmail.com>
2024-06-19 16:16:53 +0200

    migrate content from blend shapes branch

---
+++ package/Runtime/DeviceDetection/Needle.DeviceDetection~/codegen/register_types.ts
@@ -0,0 +1,7 @@
+﻿import { TypeStore } from "@needle-tools/engine"
+
+// Import types
+import { DeviceDetection } from "../DeviceDetection.js";
+
+// Register types
+TypeStore.add("DeviceDetection", DeviceDetection);
