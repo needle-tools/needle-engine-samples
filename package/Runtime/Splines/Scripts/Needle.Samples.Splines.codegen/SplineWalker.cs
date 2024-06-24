753d82d1aa180d1a4bf08cbf7f02048a87170387
Krystof <kipash612@gmail.com>
2024-06-24 19:28:58 +0200

    add RequirePackage and use it in Splines ; remove package handling code from SplineWalker
    https://linear.app/needle/issue/NE-5140/package-helper-component-allowing-for-quick-package-install

--- package/Runtime/Splines/Scripts/Needle.Samples.Splines.codegen/SplineWalker.cs
+++ package/Runtime/Splines/Scripts/Needle.Samples.Splines.codegen/SplineWalker.cs
@@ -28,34 +28,3 @@ namespace Needle.Typescript.GeneratedComponents
 }
 
 #endif
-
-#if UNITY_EDITOR
-
-namespace Needle.Typescript.GeneratedComponents
-{
-	[UnityEditor.CustomEditor(typeof(SplineWalker))]
-    public class SplineWalkerEdiotr : UnityEditor.Editor
-	{
-		UnityEngine.GUIStyle labelSkin;
-        public override void OnInspectorGUI()
-        {
-#if SPLINES_PACAKGE
-            base.OnInspectorGUI();
-#elif !UNITY_2022_1_OR_NEWER
-			if (labelSkin == null)
-			{
-                labelSkin = new UnityEngine.GUIStyle(UnityEditor.EditorStyles.boldLabel);
-				labelSkin.normal.textColor = new UnityEngine.Color(0.77f, 0.29f, 0.29f);
-            }
-
-			UnityEngine.GUILayout.Label("Splines pacakge requires Unity 2022.3+", labelSkin);
-#else
-			if (UnityEngine.GUILayout.Button("Install Splines package")) {
-				UnityEditor.PackageManager.UI.Window.Open("com.unity.splines");
-			}
-#endif
-        }
-    }
-}
-
-#endif
---
+++ package/Runtime/_Internal/RequirePackage.cs
@@ -0,0 +1,169 @@
+﻿using UnityEngine;
+using System;
+using System.Linq;
+#if UNITY_EDITOR
+using UnityEditor;
+using PM = UnityEditor.PackageManager;
+#endif
+
+namespace Needle.Engine
+{
+    public class RequirePackage : MonoBehaviour
+    {
+        public string packageName;
+
+        [ContextMenu("Add pacakge")]
+        void AddPackage()
+        {
+#if UNITY_EDITOR
+            PackageHelper.InstallPackage(packageName);
+#endif
+        }
+
+        [ContextMenu("Remove pacakge")]
+        void RemovePackage()
+        {
+#if UNITY_EDITOR
+            PackageHelper.UninstallPackage(packageName);
+#endif
+        }
+    }
+
+#if UNITY_EDITOR
+
+    [InitializeOnLoad]
+    static class PackageHelper
+    {
+        public static string[] InstalledPackages = new string[0];
+
+        static PM.Requests.Request currentRequest = null;
+        static Action<PM.Requests.Request> handler = null;
+        static PackageHelper()
+        {
+            EditorApplication.projectChanged += () => GetPackageList();
+            EditorApplication.update += Update;
+            GetPackageList();
+        }
+
+        public static bool IsProcessing => currentRequest != null && !currentRequest.IsCompleted;
+
+        static void Update()
+        {
+            if (currentRequest != null && currentRequest.IsCompleted)
+            {
+                var req = currentRequest;
+                currentRequest = null;
+
+                handler?.Invoke(req);
+                handler = null;
+            }
+        }
+
+        static void ResolveRequest(Func<PM.Requests.Request> call, Func<PM.Requests.Request, bool> handle, int iteration = 0)
+        {
+            if (iteration > 2) return;
+            if (IsProcessing) return;
+
+            currentRequest = call();
+            handler = (req) =>
+            {
+                if (req == null || req.Status != PM.StatusCode.Success || !handle(req))
+                {
+                    ResolveRequest(call, handle, ++iteration);
+                }
+            };
+        }
+
+        public static void GetPackageList(Action onCompleate = null, int iteration = 0)
+        {
+            ResolveRequest(
+                call: () => PM.Client.List(false, true),
+                handle: (req) =>
+                {
+                    var listReq = req as PM.Requests.ListRequest;
+                    if (listReq == null) return false;
+
+                    InstalledPackages = listReq.Result.Select(x => x.name).ToArray();
+                    onCompleate?.Invoke();
+
+                    return true;
+                }
+            );
+        }
+
+        public static void InstallPackage(string com, Action onCompleate = null, int iteration = 0)
+        {
+            ResolveRequest(
+                call: () => PM.Client.Add(com),
+                handle: (req) =>
+                {
+                    onCompleate?.Invoke();
+                    return true;
+                }
+            );
+        }
+
+        public static void UninstallPackage(string com, Action onCompleate = null, int iteration = 0)
+        {
+            ResolveRequest(
+                call: () => PM.Client.Remove(com),
+                handle: (req) =>
+                {
+                    onCompleate?.Invoke();
+                    return true;
+                }
+            );
+        }
+    }
+
+    [CustomEditor(typeof(RequirePackage))]
+    public class PackageRequiredEditor : Editor
+    {
+        bool? packageIsInstalled = null;
+
+        string packageName => (target as RequirePackage)?.packageName ?? string.Empty;
+
+        void OnEnable()
+        {
+            if (PackageHelper.InstalledPackages.Length == 0)
+                PackageHelper.GetPackageList(decideInstallState);
+            else
+                decideInstallState();
+        }
+
+        void decideInstallState()
+        {
+            packageIsInstalled = PackageHelper.InstalledPackages.Contains(packageName);
+        }
+
+        public override void OnInspectorGUI()
+        {
+            if (PackageHelper.IsProcessing)
+            {
+                GUILayout.Label($"Processing...");
+            }
+            else if (packageIsInstalled == null)
+            {
+                GUILayout.Label($"Fetching...");
+                decideInstallState();
+            }
+            else if (packageIsInstalled == true)
+            {
+                GUILayout.Label($"The package {packageName} is installed");
+            }
+            else if (packageIsInstalled == false)
+            {
+                GUILayout.Label($"This sample relies on {packageName}");
+
+                if (GUILayout.Button("Install package"))
+                {
+                    PackageHelper.InstallPackage(packageName, () =>
+                    {
+                        PackageHelper.GetPackageList(decideInstallState);
+                    });
+                }
+            }
+        }
+    }
+#endif
+}
