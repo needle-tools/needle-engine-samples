2f7f5463a78268edb204ad764f8c4f3458c45db0
hybridherbst <felix.herbst@gmail.com>
2024-06-25 12:20:30 +0200

    simplify package requirements script

--- package/Runtime/_Internal/RequirePackage.cs
+++ package/Runtime/_Internal/RequirePackage.cs
@@ -1,168 +1,59 @@
 ﻿using UnityEngine;
-using System;
-using System.Linq;
 #if UNITY_EDITOR
+using System.IO;
 using UnityEditor;
-using PM = UnityEditor.PackageManager;
+using UnityEditor.PackageManager;
 #endif
 
 namespace Needle.Engine
 {
-    public class RequirePackage : MonoBehaviour
+    [AddComponentMenu("")]
+    internal class RequirePackage : MonoBehaviour
     {
         public string packageName;
-
-        [ContextMenu("Add pacakge")]
-        void AddPackage()
-        {
-#if UNITY_EDITOR
-            PackageHelper.InstallPackage(packageName);
-#endif
-        }
-
-        [ContextMenu("Remove pacakge")]
-        void RemovePackage()
-        {
-#if UNITY_EDITOR
-            PackageHelper.UninstallPackage(packageName);
-#endif
-        }
     }
-
+    
 #if UNITY_EDITOR
-
-    [InitializeOnLoad]
-    static class PackageHelper
-    {
-        public static string[] InstalledPackages = new string[0];
-
-        static PM.Requests.Request currentRequest = null;
-        static Action<PM.Requests.Request> handler = null;
-        static PackageHelper()
-        {
-            EditorApplication.projectChanged += () => GetPackageList();
-            EditorApplication.update += Update;
-            GetPackageList();
-        }
-
-        public static bool IsProcessing => currentRequest != null && !currentRequest.IsCompleted;
-
-        static void Update()
-        {
-            if (currentRequest != null && currentRequest.IsCompleted)
-            {
-                var req = currentRequest;
-                currentRequest = null;
-
-                handler?.Invoke(req);
-                handler = null;
-            }
-        }
-
-        static void ResolveRequest(Func<PM.Requests.Request> call, Func<PM.Requests.Request, bool> handle, int iteration = 0)
-        {
-            if (iteration > 2) return;
-            if (IsProcessing) return;
-
-            currentRequest = call();
-            handler = (req) =>
-            {
-                if (req == null || req.Status != PM.StatusCode.Success || !handle(req))
-                {
-                    ResolveRequest(call, handle, ++iteration);
-                }
-            };
-        }
-
-        public static void GetPackageList(Action onCompleate = null, int iteration = 0)
-        {
-            ResolveRequest(
-                call: () => PM.Client.List(false, true),
-                handle: (req) =>
-                {
-                    var listReq = req as PM.Requests.ListRequest;
-                    if (listReq == null) return false;
-
-                    InstalledPackages = listReq.Result.Select(x => x.name).ToArray();
-                    onCompleate?.Invoke();
-
-                    return true;
-                }
-            );
-        }
-
-        public static void InstallPackage(string com, Action onCompleate = null, int iteration = 0)
-        {
-            ResolveRequest(
-                call: () => PM.Client.Add(com),
-                handle: (req) =>
-                {
-                    onCompleate?.Invoke();
-                    return true;
-                }
-            );
-        }
-
-        public static void UninstallPackage(string com, Action onCompleate = null, int iteration = 0)
-        {
-            ResolveRequest(
-                call: () => PM.Client.Remove(com),
-                handle: (req) =>
-                {
-                    onCompleate?.Invoke();
-                    return true;
-                }
-            );
-        }
-    }
-
     [CustomEditor(typeof(RequirePackage))]
-    public class PackageRequiredEditor : Editor
+    internal class RequirePackageEditor : Editor
     {
-        bool? packageIsInstalled = null;
-
-        string packageName => (target as RequirePackage)?.packageName ?? string.Empty;
-
-        void OnEnable()
+        private bool isInstalled = false;
+        private bool isInstalling = false;
+        internal static bool IsInstalled(RequirePackage pkg) => File.Exists($"Packages/{pkg.packageName}/package.json");
+        
+        private void OnEnable()
         {
-            if (PackageHelper.InstalledPackages.Length == 0)
-                PackageHelper.GetPackageList(decideInstallState);
-            else
-                decideInstallState();
-        }
-
-        void decideInstallState()
-        {
-            packageIsInstalled = PackageHelper.InstalledPackages.Contains(packageName);
+            isInstalled = IsInstalled(target as RequirePackage);
         }
 
         public override void OnInspectorGUI()
         {
-            if (PackageHelper.IsProcessing)
+            var t = target as RequirePackage;
+            if (!t) return;
+            
+            if (isInstalled)
+                EditorGUILayout.LabelField("✓ Package \"" + t.packageName + "\" is installed");
+            else if (isInstalling)
             {
-                GUILayout.Label($"Processing...");
+                EditorGUI.BeginDisabledGroup(true);
+                GUILayout.Button("Installing package \"" + t.packageName + "\"...");
+                EditorGUI.EndDisabledGroup();
             }
-            else if (packageIsInstalled == null)
+            else
             {
-                GUILayout.Label($"Fetching...");
-                decideInstallState();
-            }
-            else if (packageIsInstalled == true)
-            {
-                GUILayout.Label($"The package {packageName} is installed");
-            }
-            else if (packageIsInstalled == false)
-            {
-                GUILayout.Label($"This sample relies on {packageName}");
-
-                if (GUILayout.Button("Install package"))
+                EditorGUILayout.HelpBox($"The package \"{t.packageName}\" is required for this sample to work.", MessageType.Warning);
+                if (GUILayout.Button($"Install {t.packageName}"))
                 {
-                    PackageHelper.InstallPackage(packageName, () =>
-                    {
-                        PackageHelper.GetPackageList(decideInstallState);
-                    });
+                    Client.Add(t.packageName);
+                    isInstalling = true;
                 }
             }
+        }
+
+        [MenuItem("CONTEXT/RequirePackage/Uninstall Package")]
+        static void RemovePackage(MenuCommand cmd)
+        {
+            Client.Remove((cmd.context as RequirePackage)?.packageName);
         }
     }
 #endif
