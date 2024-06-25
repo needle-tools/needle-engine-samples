5d54c58925b3029be972809be0655302808cafa8
Krystof <kipash612@gmail.com>
2024-06-25 14:56:58 +0200

    migrate EverywhereConfigurator to SampleScripts npmdef ; small changes
    https://linear.app/needle/issue/NE-4711/click-through-anims-jet-engine

--- package/Runtime/JetEngine/Scripts/Needle.Samples.ProductConfigurator~/codegen/register_types.ts
+++
@@ -1,8 +0,0 @@
-﻿/* eslint-disable */
-import { TypeStore } from "@needle-tools/engine"
-
-// Import types
-import { EverywhereConfigurator } from "../EverywhereConfigurator.js";
-
-// Register types
-TypeStore.add("EverywhereConfigurator", EverywhereConfigurator);
