8d1a139ba375d88e723483ef20672a8630ec7e26
Krystof <kipash612@gmail.com>
2024-07-05 14:38:45 +0200

    add auto respawn ; expose func for setting rotY from obj rot
    https://linear.app/needle/issue/NE-5246/add-auto-reset-when-player-falls-off-the-map

--- package/Runtime/FirstPersonController/Scripts/FirstPersonController~/Networking/SpawnHandler.ts
+++ package/Runtime/FirstPersonController/Scripts/FirstPersonController~/Networking/SpawnHandler.ts
@@ -1,4 +1,4 @@
-import { Behaviour, GameObject, Gizmos, RaycastOptions, delayForFrames, getParam, getTempQuaternion, getTempVector, serializable } from "@needle-tools/engine";
+import { Behaviour, GameObject, Gizmos, RaycastOptions, delayForFrames, getParam, getTempQuaternion, getTempVector, serializable, setWorldPosition, setWorldQuaternion } from "@needle-tools/engine";
 import { Vector3, Object3D, Ray, Layers } from "three";
 import { FirstPersonController } from "../FirstPersonCharacter";
 import { MobileControls } from "../MobileControls";
@@ -7,19 +7,40 @@ const debug = getParam("debugspawnhandler")
 
 export class SpawnHandler extends Behaviour {
 
-    //array of Object3D
     @serializable(Object3D)
     spawnPoints: Object3D[] = [];
 
     @serializable(MobileControls)
     mobileControls?: MobileControls;
 
+    @serializable()
+    autoRespawn: boolean = true;
+
+    @serializable()
+    autoRespawnLimitY: number = -10;
+
     private downVector = new Vector3(0, -1, 0);
 
     async handlePlayerSpawn(obj: GameObject) {
-        // HACK: delay for one frame since otherwise the newly created collider gets read back and overrides any value we set to the object
-        await delayForFrames(1); 
+        // HACK: delay for one frame otherwise the newly created collider gets read back (?) and overrides any value we set to the object
+        await delayForFrames(1);
+        
+        const spot = this.getRandomSpawnPoint();
+        this.setPlayerToSpot(obj, spot);
 
+        this.handlePlayerAutoRespawn(obj);
+
+        // hook touch controls to the spawned player
+        const player = (obj as GameObject)?.getComponent(FirstPersonController);
+        if (player && this.mobileControls) {
+            this.mobileControls.onLook.addEventListener(v => player.look(v));
+            this.mobileControls.onMove.addEventListener(v => player.move(v));
+            this.mobileControls.onJump.addEventListener(() => player.jump());
+        }
+    }
+
+    // TODO: add network authoring
+    getRandomSpawnPoint(): Object3D | undefined {
         //shuffle spawnspots
         this.spawnPoints.sort(() => Math.random() - 0.5);
 
@@ -51,18 +72,27 @@ export class SpawnHandler extends Behaviour {
             }
         }
 
-        // If there is no valid spawn point, set world 0,0,0
-        if (spot && obj) {
-            obj.worldPosition = spot.getWorldPosition(getTempVector());
-            obj.worldQuaternion = spot.getWorldQuaternion(getTempQuaternion());
-        }
+        return spot;
+    }
 
-        // hook touch controls to the spawned player
-        const player = (obj as GameObject)?.getComponent(FirstPersonController);
-        if (player && this.mobileControls) {
-            this.mobileControls.onLook.addEventListener(v => player.look(v));
-            this.mobileControls.onMove.addEventListener(v => player.move(v));
-            this.mobileControls.onJump.addEventListener(() => player.jump());
+    setPlayerToSpot(instance: Object3D, spot: Object3D | undefined) {
+        if (!instance || !spot) return;
+
+        setWorldPosition(instance, spot.getWorldPosition(getTempVector()));
+        setWorldQuaternion(instance, spot.getWorldQuaternion(getTempQuaternion()));
+        GameObject.getComponent(instance, FirstPersonController)?.setCamRotationFromObject();
+    }
+
+    /* Watch player Y position and respawn if invalid */
+    async handlePlayerAutoRespawn(instance: Object3D) {
+        while (instance) {
+            await delayForFrames(1);
+
+            const wp = instance.getWorldPosition(getTempVector());
+            if (wp.y < this.autoRespawnLimitY) {
+                const spot = this.getRandomSpawnPoint();
+                this.setPlayerToSpot(instance, spot);
+            }
         }
     }
 }
