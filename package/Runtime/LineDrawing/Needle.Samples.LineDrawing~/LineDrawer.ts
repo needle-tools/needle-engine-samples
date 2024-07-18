8e2bfa3cc4e315c4c46341d6bcaf4a44d65d64da
Marcel Wiessler <marcel@gaisterhand.de>
2024-07-18 12:38:57 +0200

    LineDrawing: enable depth sensing, update scene, enable spatial drawing

--- package/Runtime/LineDrawing/Needle.Samples.LineDrawing~/LineDrawer.ts
+++ package/Runtime/LineDrawing/Needle.Samples.LineDrawing~/LineDrawer.ts
@@ -1,7 +1,6 @@
-import { Behaviour, GameObject, NEPointerEvent, getTempVector, serializable } from "@needle-tools/engine";
-import { RaycastOptions, Mathf, getWorldPosition, PlayerColor } from "@needle-tools/engine";
+import { Behaviour, GameObject, NEPointerEvent, serializable, RaycastOptions, Mathf, getWorldPosition, PlayerColor } from "@needle-tools/engine";
 import * as THREE from 'three';
-import { Object3D, Color, Ray, Raycaster, Vector3 } from "three";
+import { Object3D, Color, Vector3 } from "three";
 import { LineHandle, LinesManager } from "./LinesManager";
 import { MeshLineMaterial } from 'three.meshline';
 
@@ -45,50 +44,46 @@ export class LinesDrawer extends Behaviour {
     onDisable(): void {
         this.context.input.removeEventListener("pointerdown", this._onPointerDown);
         this.context.input.removeEventListener("pointermove", this._onPointerMove);
-        this.context.input.removeEventListener("pointerup", this._onPointerUp);        
+        this.context.input.removeEventListener("pointerup", this._onPointerUp);
     }
 
     private _onPointerDown = (args: NEPointerEvent) => {
-        if(args.button !== 0) return;
+        if (args.button !== 0) return;
     }
 
     private _onPointerMove = (args: NEPointerEvent) => {
-        if(args.button !== 0) return;
+        if (args.button !== 0) return;
 
-        if(!this.context.input.getPointerPressed(args.pointerId)) return;
+        if (!this.context.input.getPointerPressed(args.pointerId)) return;
 
         this.onPointerUpdate(args);
     }
 
     private _onPointerUp = (args: NEPointerEvent) => {
-        if(args.button !== 0) return;
+        if (args.button !== 0) return;
 
         this.onPointerUpdate(args);
     }
 
     private onPointerUpdate(args: NEPointerEvent) {
-        const pos = args.space.getWorldPosition(getTempVector());
-        const rot = args.space.getWorldDirection(getTempVector());
-        const ray = new Ray(pos, rot);
-
         const finish = this.context.input.getPointerUp(args.pointerId);
-
-        this.updateLine(args.pointerId.toString(), ray, true, finish, false);
+        const isSpatialDevice = args.isSpatial;
+        this.updateLine(args.pointerId.toString(), isSpatialDevice, args.ray, true, finish, false);
     }
 
     start() {
         if (!this.lines) {
             this.lines = GameObject.getComponent(this.gameObject, LinesManager)!;
             if (!this.lines)
-                this.lines = GameObject.addNewComponent(this.gameObject, LinesManager);
+                this.lines = GameObject.addComponent(this.gameObject, LinesManager);
         }
-        
+
         this._states["mouse"] = new LineState();
     }
 
     private _states: { [id: string]: LineState } = {};
 
-    private updateLine(id: string, ray: THREE.Ray, active: boolean, finish: boolean, cancel: boolean = false): LineState {
+    private updateLine(id: string, isSpatial: boolean, ray: THREE.Ray, active: boolean, finish: boolean, cancel: boolean = false): LineState {
         let state = this._states[id];
         if (!state) {
             this._states[id] = new LineState();
@@ -107,50 +102,61 @@ export class LinesDrawer extends Behaviour {
             if (cancel) {
                 return state;
             }
-            const hit = this.getHit(ray);
             let pt: THREE.Vector3 | null = null;
+            let hitParent: THREE.Object3D | null = null;
             let prev = state.prevDistance;
-            if(state.maxDistance === 0) {
+            if (state.maxDistance === 0) {
                 state.maxDistance = this.context.mainCamera?.getWorldPosition(new Vector3()).length() ?? 0;
                 prev = state.maxDistance;
             }
-            if (hit) {                
-                if (!state.currentHandle) {
-                    state.maxDistance = hit.distance;
-                }
-                pt = hit.point;
-                if (hit.face)
-                    pt.add(hit.face.normal.multiplyScalar(0.01));
-                state.prevDistance = hit.distance;
+
+            if (isSpatial) {
+                const xrScale = this.context.xr?.rigScale || 1;
+                const dist = .01 * xrScale;
+                pt = ray.origin.add(ray.direction.multiplyScalar(dist));
+                // this controls how many points are drawn per unit of distance
+                state.prevDistance = xrScale * .003;
             }
-            else if (state.maxDistance > 0) {
-                let dist = state.maxDistance;
-                // if we start drawing in thin air:
-                if (!state.currentHandle && state.lastHit) {
-                    const wp = getWorldPosition(this.context.mainCamera!);
-                    dist = state.lastHit.distanceTo(wp);
+            else {
+                const hit = this.getHit(ray);
+                if (hit) {
+                    if (!state.currentHandle) {
+                        state.maxDistance = hit.distance;
+                    }
+                    pt = hit.point;
+                    if (hit.face)
+                        pt.add(hit.face.normal.multiplyScalar(0.01));
+                    state.prevDistance = hit.distance;
+                    hitParent = hit.object;
                 }
-                pt = ray.origin.add(ray.direction.multiplyScalar(state.maxDistance));
-                state.prevDistance = state.maxDistance;
+                else if (state.maxDistance > 0) {
+                    let dist = state.maxDistance;
+                    // if we start drawing in thin air:
+                    if (!state.currentHandle && state.lastHit) {
+                        const wp = getWorldPosition(this.context.mainCamera!);
+                        dist = state.lastHit.distanceTo(wp);
+                    }
+                    pt = ray.origin.add(ray.direction.multiplyScalar(state.maxDistance));
+                    state.prevDistance = state.maxDistance;
+                }
             }
 
             if (pt) {
                 // abort the draw if the drawn segment is too long
-                if(state.lastHit.distanceTo(pt) > 6) {
+                if (state.lastHit.distanceTo(pt) > 6) {
                     if (state.currentHandle) {
                         // this.sendLineUpdate();
                         this.lines.endLine(state.currentHandle);
                         state.currentHandle = null;
-
                         return state;
                     }
                 }
 
                 if (!state.currentHandle) {
-                    let parent = state.lastParent ?? this.gameObject as THREE.Object3D;
-                    if (this.addToPaintedObject && hit) parent = hit.object;
-                    state.lastParent = parent;
-                    state.currentHandle = this.lines.startLine(parent, { material: this.createRandomMaterial() });
+                    let lineParent = state.lastParent ?? this.gameObject as THREE.Object3D;
+                    if (this.addToPaintedObject && hitParent) lineParent = hitParent;
+                    state.lastParent = lineParent;
+                    state.currentHandle = this.lines.startLine(lineParent, { material: this.createRandomMaterial() });
                 }
 
                 if (this.alignToSurface) {
@@ -177,6 +183,13 @@ export class LinesDrawer extends Behaviour {
     private getHit(ray: THREE.Ray): THREE.Intersection | null {
         if (!this.colliders || this.colliders.length === 0) {
             this.colliders = [this.gameObject];
+        }
+        // remove invalid entries
+        for (let i = this.colliders.length - 1; i >= 0; i--) {
+            const entry = this.colliders[i];
+            if (!entry) {
+                this.colliders.splice(i, 1);
+            }
         }
         this._raycastOptions.targets = this.colliders;
         this._raycastOptions.ray = ray;
