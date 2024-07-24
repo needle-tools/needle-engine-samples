46875d339fa374d5eefe5235b3b2ca1053ff1c72
hybridherbst <felix.herbst@gmail.com>
2024-07-24 18:08:17 +0200

    move custom depth sensing files

--- package/Runtime/DepthSensing/Needle.Sample.DepthSensing~/codegen/register_types.ts
+++
@@ -1,8 +0,0 @@
-﻿/* eslint-disable */
-import { TypeStore } from "@needle-tools/engine"
-
-// Import types
-import { CustomDepthSensing } from "../CustomDepthSensing.js";
-
-// Register types
-TypeStore.add("CustomDepthSensing", CustomDepthSensing);
