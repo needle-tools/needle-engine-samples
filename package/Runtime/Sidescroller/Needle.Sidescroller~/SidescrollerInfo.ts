600b51a8f337ce13ff085592422bc02ab80c93aa
Krystof <kipash612@gmail.com>
2024-09-02 14:25:27 +0200

    Sidescroller: disable progressive ; add font for custom html

--- package/Runtime/Sidescroller/Needle.Sidescroller~/SidescrollerInfo.ts
+++ package/Runtime/Sidescroller/Needle.Sidescroller~/SidescrollerInfo.ts
@@ -5,7 +5,7 @@ import { Behaviour } from "@needle-tools/engine";
 export class SidescrollerInfo extends Behaviour {
 
     private template = `
-    <div style="position: absolute; left: 20px; top: 20px; z-index: 1000; color: white; font-size: 0.8em; opacity: 0.8; background-color: #00000099; border-radius: 30px; padding: 15px; user-select: text; margin-right: 20px;">
+    <div style="position: absolute; left: 20px; top: 20px; z-index: 1000; color: white; font-size: 0.8em; opacity: 0.8; background-color: #00000099; border-radius: 30px; padding: 15px; user-select: text; margin-right: 20px; font-family: Roboto, sans-serif;">
         <button class="close" style="background: white; border: none; border-radius: 30px; width: 30px; height: 30px;"><span style="font-size: 2em; line-height:0; vertical-align: sub; text-align: center;">×</span></button>
         <button class="fullscreen" style="background: white; border: none; border-radius: 30px; width: 30px; height: 30px;"><span style="font-size: 1.2em; line-height:0; vertical-align: middle; text-align: center;">↗</span></button>
         <div class="content">
