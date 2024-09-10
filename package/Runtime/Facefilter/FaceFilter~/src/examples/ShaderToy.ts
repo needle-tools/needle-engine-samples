e349757a96020b0449a9e8d095983c661c6d619d
Marcel Wiessler <marcel@gaisterhand.de>
2024-09-10 20:40:47 +0200

    add experimental shadertoy facemesh filter

---
+++ package/Runtime/Facefilter/FaceFilter~/src/examples/ShaderToy.ts
@@ -0,0 +1,143 @@
+import { Behaviour, Context, getParam, setParam, setParamWithoutReload, showBalloonWarning } from "@needle-tools/engine";
+import { FaceMeshBehaviour } from "../facemesh/FaceMeshBehaviour";
+import { Material, ShaderMaterial, ShaderMaterialParameters, Vector3, Vector4 } from "three";
+
+/*
+Shader Inputs
+uniform vec3      iResolution;           // viewport resolution (in pixels)
+uniform float     iTime;                 // shader playback time (in seconds)
+uniform float     iTimeDelta;            // render time (in seconds)
+uniform float     iFrameRate;            // shader frame rate
+uniform int       iFrame;                // shader playback frame
+uniform float     iChannelTime[4];       // channel playback time (in seconds)
+uniform vec3      iChannelResolution[4]; // channel resolution (in pixels)
+uniform vec4      iMouse;                // mouse pixel coords. xy: current (if MLB down), zw: click
+uniform samplerXX iChannel0..3;          // input channel. XX = 2D/Cube
+uniform vec4      iDate;                 // (year, month, day, time in seconds)
+*/
+
+const inputsChunk = `
+uniform vec3      iResolution;           // viewport resolution (in pixels)
+uniform float     iTime;                 // shader playback time (in seconds)
+uniform float     iTimeDelta;            // render time (in seconds)
+uniform float     iFrameRate;            // shader frame rate
+uniform int       iFrame;                // shader playback frame
+// channelPlaybackTime
+uniform vec3      iChannelResolution[4]; // channel resolution (in pixels)
+uniform vec4      iMouse;                // mouse pixel coords. xy: current (if MLB down), zw: click
+uniform vec4      iDate;                 // (year, month, day, time in seconds)
+uniform sampler2D iChannel0;
+`
+
+const mainChunk = `
+void main() {
+    gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
+    mainImage(gl_FragColor, gl_FragCoord.xy);
+}
+`
+
+const fragmentShader = `
+void mainImage( out vec4 fragColor, in vec2 fragCoord )
+{
+    // Normalized pixel coordinates (from 0 to 1)
+    vec2 uv = fragCoord/iResolution.xy;
+
+    // Time varying pixel color
+    vec3 col = 0.5 + 0.5*cos(iTime+uv.xyx+vec3(0,2,4));
+
+    // Output to screen
+    fragColor = vec4(col,1.0);
+}
+`;
+
+
+class ShaderToyMaterial extends ShaderMaterial {
+    constructor(args?: ShaderMaterialParameters) {
+        super(args);
+        if (!args?.fragmentShader) {
+            let shader = inputsChunk;
+            shader += fragmentShader;
+            shader += mainChunk;
+            this.fragmentShader = shader;
+        }
+        this.uniforms = {
+            iResolution: { value: new Vector3(window.innerWidth, window.innerHeight, 1) },
+            iTime: { value: 0 },
+            iTimeDelta: { value: 0 },
+            iFrameRate: { value: 0 },
+            iFrame: { value: 0 },
+            iChannelTime: { value: [0, 0, 0, 0] },
+            iChannelResolution: { value: [new Vector3(1, 1, 1), new Vector3(1, 1, 1), new Vector3(1, 1, 1), new Vector3(1, 1, 1)] },
+            iMouse: { value: new Vector4(0, 0, 0, 0) },
+            iDate: { value: new Vector4(0, 0, 0, 0) },
+            iChannel0: { value: null },
+            iChannel1: { value: null },
+            iChannel2: { value: null },
+            iChannel3: { value: null },
+            ...this.uniforms,
+        }
+    }
+    update(filter: ShaderToyFaceFilter) {
+        const context = filter.context;
+        this.uniforms.iResolution.value.set(context.domWidth, context.domHeight, 1);
+        this.uniforms.iTime.value = context.time.realtimeSinceStartup;
+        this.uniforms.iTimeDelta.value = context.time.deltaTime;
+        this.uniforms.iFrameRate.value = context.time.smoothedFps;
+        this.uniforms.iFrame.value = context.time.frameCount;
+        this.uniforms.iChannelTime.value[0] = context.time.realtimeSinceStartup;
+        // this.uniforms.iChannelResolution.value[0].set(context.domWidth, context.domHeight, 1);
+        const pointerPosition = context.input.getPointerPosition(0);
+        if (pointerPosition)
+            this.uniforms.iMouse.value.set(pointerPosition.x, pointerPosition.y, context.input.getPointerDown(0), 0);
+        const time = new Date();
+        this.uniforms.iDate.value.set(time.getFullYear(), time.getMonth(), time.getDate(), time.getTime());
+    }
+}
+
+
+export class ShaderToyFaceFilter extends FaceMeshBehaviour {
+    protected createMaterial(): Material | null {
+        return new ShaderToyMaterial()
+    }
+    update(): void {
+        const material = this.material as ShaderToyMaterial;
+        if (material) {
+            material.update(this)
+        }
+    }
+    onEnable(): void {
+        super.onEnable();
+        window.addEventListener("paste", this.onPaste);
+        const shader = getParam("shader");
+        if (typeof shader === "string" && shader.length > 1) {
+            const shaderText = atob(shader);
+            this.trySetShader(shaderText);
+        }
+    }
+    onDisable(): void {
+        super.onDisable();
+        window.removeEventListener("paste", this.onPaste);
+    }
+    private onPaste = (e: ClipboardEvent) => {
+        if (!e.clipboardData) return;
+        const text = e.clipboardData.getData("text");
+        if (text) {
+            this.trySetShader(text);
+        }
+    }
+    private trySetShader(shader: string) {
+        if (shader.includes("void mainImage")) {
+            const material = this.material as ShaderToyMaterial;
+            if (material) {
+                material.fragmentShader = inputsChunk + shader + mainChunk;
+                material.needsUpdate = true;
+
+                const base64str = btoa(shader);
+                setParamWithoutReload("shader", base64str);
+            }
+        }
+        else {
+            showBalloonWarning("The pasted text does not contain a mainImage function / is not a ShaderToy shader.")
+        }
+    }
+}
