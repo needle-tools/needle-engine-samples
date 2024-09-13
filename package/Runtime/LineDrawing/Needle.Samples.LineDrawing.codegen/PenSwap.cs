452e906e6d289b67412f47f6491746199ad88446
Krystof <kipash612@gmail.com>
2024-09-13 16:14:34 +0200

    add MXInk ; add particles ; add polar circle ; add thumbnail
    https://linear.app/needle/issue/NE-5639/

---
+++ package/Runtime/LineDrawing/Needle.Samples.LineDrawing.codegen/PenSwap.cs
@@ -0,0 +1,19 @@
+// NEEDLE_CODEGEN_START
+// auto generated code - do not edit directly
+
+#pragma warning disable
+
+namespace Needle.Typescript.GeneratedComponents
+{
+	public partial class PenSwap : UnityEngine.MonoBehaviour
+	{
+		public UnityEngine.Animator[] @animator;
+		public UnityEngine.GameObject[] @enabledForPen;
+		public UnityEngine.GameObject[] @enabledForSword;
+		public UnityEngine.Behaviour[] @enabledForPen2;
+		public UnityEngine.Behaviour[] @enabledForSword2;
+		public void update(){}
+	}
+}
+
+// NEEDLE_CODEGEN_END
--- package/Runtime/LineDrawing/Needle.Samples.LineDrawing~/codegen/register_types.ts
+++ package/Runtime/LineDrawing/Needle.Samples.LineDrawing~/codegen/register_types.ts
@@ -6,9 +6,11 @@ import { LinesDrawer } from "../LineDrawer.js";
 import { LineInstanceHandler } from "../LinesManager.js";
 import { BrushModel } from "../LinesManager.js";
 import { LinesManager } from "../LinesManager.js";
+import { PenSwap } from "../PenSwap.js";
 
 // Register types
 TypeStore.add("LinesDrawer", LinesDrawer);
 TypeStore.add("LineInstanceHandler", LineInstanceHandler);
 TypeStore.add("BrushModel", BrushModel);
 TypeStore.add("LinesManager", LinesManager);
+TypeStore.add("PenSwap", PenSwap);
