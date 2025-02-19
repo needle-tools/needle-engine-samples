67d148f750127bafebe9f5eb7a8b16b692424b41
hybridherbst <felix.herbst@gmail.com>
2025-02-19 17:08:32 +0100

    update Diamond script

--- package/Runtime/Jewelry/Scripts/Needle.Jewelry~/Diamond.ts
+++ package/Runtime/Jewelry/Scripts/Needle.Jewelry~/Diamond.ts
@@ -1,14 +1,6 @@
-import { Behaviour, GameObject, USDZExporter, isQuest, serializable } from "@needle-tools/engine";
-import { Context } from "@needle-tools/engine";
-import { isiOS, isMobileDevice } from "@needle-tools/engine";
+import { Context, Behaviour, DeviceUtilities, GameObject, USDZExporter, delay, serializable } from "@needle-tools/engine";
 import { Object3D, ShaderMaterial, Vector2, Color, Mesh, BufferGeometry } from "three"
-import { shaderIntersectFunction } from "three-mesh-bvh";
-import { shaderStructs, MeshBVHUniformStruct, MeshBVH, SAH } from "three-mesh-bvh";
-
-// extend the types scine it is missing
-declare module "three-mesh-bvh" {
-	export const shaderIntersectFunction: string;
-}
+import { shaderStructs, shaderIntersectFunction, MeshBVHUniformStruct, MeshBVH, SAH } from "three-mesh-bvh";
 
 export class Diamond extends Behaviour {
 
@@ -50,7 +42,7 @@ export class Diamond extends Behaviour {
 	}
 
     private static isMobile() {
-        return isiOS() || isMobileDevice() || isQuest();
+        return DeviceUtilities.isiOS() || DeviceUtilities.isMobileDevice() || DeviceUtilities.isQuest();
     }
 
 	private static _diamondMaterial: ShaderMaterial | undefined;
@@ -70,10 +62,13 @@ export class Diamond extends Behaviour {
 			}
 			const mat = this.getDiamondMaterial(context);
             const bvh = this._mesh.get(mesh.geometry)!;
-			mat.uniforms.bvh.value.updateFrom(bvh);
 			mesh = new Mesh();
 			mesh.geometry = bvh.geometry;
-			mesh["material"] = mat;
+			if (mat) {
+				mat.uniforms.bvh.value.updateFrom(bvh);
+				mat.needsUpdate = true;
+				mesh["material"] = mat;
+			}
 			return mesh;
 		}
 		else if (obj.type === "Group") {
@@ -107,9 +102,11 @@ export class Diamond extends Behaviour {
 			uniforms: {
 				// scene / geometry information
 				envMap: { value: context.scene.environment },
+				correctMips: { value: false },
+				fresnel: { value: 0 },
 				bvh: { value: new MeshBVHUniformStruct() },
-				projectionMatrixInv: { value: cam?.projectionMatrixInverse },
-				viewMatrixInv: { value: cam?.matrixWorld },
+				projectionMatrixInverse: { value: cam?.projectionMatrixInverse },
+				viewMatrixInverse: { value: cam?.matrixWorld },
 				resolution: { value: new Vector2(100, 100) },
 
 				// internal reflection settings
@@ -120,125 +117,160 @@ export class Diamond extends Behaviour {
 				color: { value: new Color(1, 1, 1) },
 				fastChroma: { value: fastChroma },
 				aberrationStrength: { value: aberrationStrength },
-
+			},
+			defines: {
+				"CHROMATIC_ABERRATIONS": !this.isMobile(),
 			},
 			vertexShader: /*glsl*/ `
-			varying vec3 vWorldPosition;
-			varying vec3 vNormal;
-			uniform mat4 viewMatrixInv;
-			void main() {
-				vWorldPosition = ( modelMatrix * vec4( position, 1.0 ) ).xyz;
-				vNormal = ( viewMatrixInv * vec4( normalMatrix * normal, 0.0 ) ).xyz;
-				gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4( position , 1.0 );
-			}
+				uniform mat4 viewMatrixInverse;
+
+				varying vec3 vWorldPosition;
+				varying vec3 vNormal;
+				varying mat4 vModelMatrixInverse;
+
+				#include <color_pars_vertex>
+
+				void main() {
+					#include <color_vertex>
+
+					vec4 transformedNormal = vec4(normal, 0.0);
+					vec4 transformedPosition = vec4(position, 1.0);
+					#ifdef USE_INSTANCING
+					transformedNormal = instanceMatrix * transformedNormal;
+					transformedPosition = instanceMatrix * transformedPosition;
+					#endif
+
+					#ifdef USE_INSTANCING
+					vModelMatrixInverse = inverse(modelMatrix * instanceMatrix);
+					#else
+					vModelMatrixInverse = inverse(modelMatrix);
+					#endif
+
+					vWorldPosition = (modelMatrix * transformedPosition).xyz;
+					vNormal = normalize((viewMatrixInverse * vec4(normalMatrix * transformedNormal.xyz, 0.0)).xyz);
+					gl_Position = projectionMatrix * viewMatrix * modelMatrix * transformedPosition;
+		}
 		`,
-			fragmentShader: /*glsl*/ `
-			#define RAY_OFFSET 0.0001
-			#include <common>
+			fragmentShader:   /*glsl*/ `
+			#define ENVMAP_TYPE_CUBE_UV
 			precision highp isampler2D;
 			precision highp usampler2D;
-			// ${shaderStructs}
-			// ${shaderIntersectFunction}
 			varying vec3 vWorldPosition;
 			varying vec3 vNormal;
-			uniform sampler2D envMap;
+			varying mat4 vModelMatrixInverse;
+		  
+			#include <color_pars_fragment>
+		  
+			#ifdef ENVMAP_TYPE_CUBEM
+			  uniform samplerCube envMap;
+			#else
+			  uniform sampler2D envMap;
+			#endif
+		  
 			uniform float bounces;
+			// ${shaderStructs}
+			// ${shaderIntersectFunction}
 			uniform BVH bvh;
 			uniform float ior;
-			uniform vec3 color;
-			uniform bool fastChroma;
-			uniform mat4 projectionMatrixInv;
-			uniform mat4 viewMatrixInv;
-			uniform mat4 modelMatrix;
+			// uniform bool correctMips;
 			uniform vec2 resolution;
+			uniform float fresnel;
+			uniform mat4 modelMatrix;
+			uniform mat4 projectionMatrixInverse;
+			uniform mat4 viewMatrixInverse;
 			uniform float aberrationStrength;
-			#include <cube_uv_reflection_fragment>
-			// performs an iterative bounce lookup modeling internal reflection and returns
-			// a final ray direction.
-			vec3 totalInternalReflection( vec3 incomingOrigin, vec3 incomingDirection, vec3 normal, float ior, mat4 modelMatrixInverse ) {
-				vec3 rayOrigin = incomingOrigin;
-				vec3 rayDirection = incomingDirection;
-				// refract the ray direction on the way into the diamond and adjust offset from
-				// the diamond surface for raytracing
-				rayDirection = refract( rayDirection, normal, 1.0 / ior );
-				rayOrigin = vWorldPosition + rayDirection * RAY_OFFSET;
-				// transform the ray into the local coordinates of the model
-				rayOrigin = ( modelMatrixInverse * vec4( rayOrigin, 1.0 ) ).xyz;
-				rayDirection = normalize( ( modelMatrixInverse * vec4( rayDirection, 0.0 ) ).xyz );
-				// perform multiple ray casts
-				for( float i = 0.0; i < bounces; i ++ ) {
-					// results
-					uvec4 faceIndices = uvec4( 0u );
-					vec3 faceNormal = vec3( 0.0, 0.0, 1.0 );
-					vec3 barycoord = vec3( 0.0 );
-					float side = 1.0;
-					float dist = 0.0;
-					// perform the raycast
-					// the diamond is a water tight model so we assume we always hit a surface
-					bvhIntersectFirstHit( bvh, rayOrigin, rayDirection, faceIndices, faceNormal, barycoord, side, dist );
-					// derive the new ray origin from the hit results
-					vec3 hitPos = rayOrigin + rayDirection * dist;
-					// if we don't internally reflect then end the ray tracing and sample
-					vec3 refractedDirection = refract( rayDirection, faceNormal, ior );
-					bool totalInternalReflection = length( refract( rayDirection, faceNormal, ior ) ) == 0.0;
-					if ( ! totalInternalReflection ) {
-						rayDirection = refractedDirection;
-						break;
-					}
-					// otherwise reflect off the surface internally for another hit
-					rayDirection = reflect( rayDirection, faceNormal );
-					rayOrigin = hitPos + rayDirection * RAY_OFFSET;
-				}
-				// return the final ray direction in world space
-				return normalize( ( modelMatrix * vec4( rayDirection, 0.0 ) ).xyz );
+			uniform vec3 color;
+			uniform float opacity;
+		  
+			float fresnelFunc(vec3 viewDirection, vec3 worldNormal) {
+			  return pow( 1.0 + dot( viewDirection, worldNormal), 10.0 );
 			}
-			vec4 envSample( sampler2D envMap, vec3 rayDirection ) {
+		  
+			vec3 totalInternalReflection(vec3 ro, vec3 rd, vec3 normal, float ior, mat4 modelMatrixInverse) {
+			  vec3 rayOrigin = ro;
+			  vec3 rayDirection = rd;
+			  rayDirection = refract(rayDirection, normal, 1.0 / ior);
+			  rayOrigin = vWorldPosition + rayDirection * 0.001;
+			  rayOrigin = (modelMatrixInverse * vec4(rayOrigin, 1.0)).xyz;
+			  rayDirection = normalize((modelMatrixInverse * vec4(rayDirection, 0.0)).xyz);
+			  for(float i = 0.0; i < bounces; i++) {
+				uvec4 faceIndices = uvec4( 0u );
+				vec3 faceNormal = vec3( 0.0, 0.0, 1.0 );
+				vec3 barycoord = vec3( 0.0 );
+				float side = 1.0;
+				float dist = 0.0;
+				bvhIntersectFirstHit( bvh, rayOrigin, rayDirection, faceIndices, faceNormal, barycoord, side, dist );
+				vec3 hitPos = rayOrigin + rayDirection * max(dist - 0.001, 0.0);
+				vec3 tempDir = refract(rayDirection, faceNormal, ior);
+				if (length(tempDir) != 0.0) {
+				  rayDirection = tempDir;
+				  break;
+				}
+				rayDirection = reflect(rayDirection, faceNormal);
+				rayOrigin = hitPos + rayDirection * 0.01;
+			  }
+			  rayDirection = normalize((modelMatrix * vec4(rayDirection, 0.0)).xyz);
+			  return rayDirection;
+			}
+		  
+			#include <common>
+			// #include <cube_uv_reflection_fragment>
+		  
+			#ifdef ENVMAP_TYPE_CUBEM
+			  vec4 textureGradient(samplerCube envMap, vec3 rayDirection, vec3 directionCamPerfect) {
+				vec2 dx = dFdx(directionCamPerfect); // dFdx(correctMips ? directionCamPerfect: rayDirection);
+				vec2 dy = dFdy(directionCamPerfect); // dFdy(correctMips ? directionCamPerfect: rayDirection);
+			  	return textureGrad(envMap, rayDirection, dx, dy);
+			  }
+			#else
+			  vec4 textureGradient(sampler2D envMap, vec3 rayDirection, vec3 directionCamPerfect) {
 				vec2 uvv = equirectUv( rayDirection );
-				return texture( envMap, uvv );
-			}
+				vec2 smoothUv = equirectUv( directionCamPerfect );
+				vec2 dx = dFdx(smoothUv); // dFdx(correctMips ? smoothUv : uvv);
+				vec2 dy = dFdy(smoothUv); // dFdy(correctMips ? smoothUv : uvv);
+				return textureGrad(envMap, uvv, dx, dy);
+			  }
+			#endif
+		  
 			void main() {
-				mat4 modelMatrixInverse = inverse( modelMatrix );
-				vec2 uv = gl_FragCoord.xy / resolution;
-				vec3 normal = vNormal;
-				vec3 rayOrigin = cameraPosition;
-				vec3 rayDirection = normalize( vWorldPosition - cameraPosition );
-				if ( aberrationStrength != 0.0 ) {
-					// perform chromatic aberration lookups
-					vec3 rayDirectionG = totalInternalReflection( rayOrigin, rayDirection, normal, max( ior, 1.0 ), modelMatrixInverse );
-					vec3 rayDirectionR, rayDirectionB;
-					if ( fastChroma ) {
-						// fast chroma does a quick uv offset on lookup
-						rayDirectionR = normalize( rayDirectionG + 1.0 * vec3( aberrationStrength / 2.0 ) );
-						rayDirectionB = normalize( rayDirectionG - 1.0 * vec3( aberrationStrength / 2.0 ) );
-					} else {
-						// compared to a proper ray trace of diffracted rays
-						float iorR = max( ior * ( 1.0 - aberrationStrength ), 1.0 );
-						float iorB = max( ior * ( 1.0 + aberrationStrength ), 1.0 );
-						rayDirectionR = totalInternalReflection(
-							rayOrigin, rayDirection, normal,
-							iorR, modelMatrixInverse
-						);
-						rayDirectionB = totalInternalReflection(
-							rayOrigin, rayDirection, normal,
-							iorB, modelMatrixInverse
-						);
-					}
-					// get the color lookup
-					float r = envSample( envMap, rayDirectionR ).r;
-					float g = envSample( envMap, rayDirectionG ).g;
-					float b = envSample( envMap, rayDirectionB ).b;
-					gl_FragColor.rgb = vec3( r, g, b ) * color;
-					gl_FragColor.a = 1.0;
-				} else {
-					// no chromatic aberration lookups
-					rayDirection = totalInternalReflection( rayOrigin, rayDirection, normal, max( ior, 1.0 ), modelMatrixInverse );
-					gl_FragColor.rgb = envSample( envMap, rayDirection ).rgb * color;
-					gl_FragColor.a = 1.0;
-				}
-				#include <tonemapping_fragment>
-				#include <colorspace_fragment>
-			}
-		`
+			  vec2 uv = gl_FragCoord.xy / resolution;
+
+			  vec3 directionCamPerfect = (projectionMatrixInverse * vec4(uv * 2.0 - 1.0, 0.0, 1.0)).xyz;
+			  directionCamPerfect = (viewMatrixInverse * vec4(directionCamPerfect, 0.0)).xyz;
+			  directionCamPerfect = normalize(directionCamPerfect);
+			  vec3 normal = vNormal;
+			  vec3 rayOrigin = cameraPosition;
+			  vec3 rayDirection = normalize(vWorldPosition - cameraPosition);
+		  
+			  vec4 diffuseColor = vec4(color, opacity);
+			  #include <color_fragment>
+
+			  #ifdef CHROMATIC_ABERRATIONS
+				vec3 rayDirectionG = totalInternalReflection(rayOrigin, rayDirection, normal, max(ior, 1.0), vModelMatrixInverse);
+				#ifdef FAST_CHROMA
+				  vec3 rayDirectionR = normalize(rayDirectionG + 1.0 * vec3(aberrationStrength / 2.0));
+				  vec3 rayDirectionB = normalize(rayDirectionG - 1.0 * vec3(aberrationStrength / 2.0));
+				#else
+				  vec3 rayDirectionR = totalInternalReflection(rayOrigin, rayDirection, normal, max(ior * (1.0 - aberrationStrength), 1.0), vModelMatrixInverse);
+				  vec3 rayDirectionB = totalInternalReflection(rayOrigin, rayDirection, normal, max(ior * (1.0 + aberrationStrength), 1.0), vModelMatrixInverse);
+				#endif
+				float finalColorR = textureGradient(envMap, rayDirectionR, directionCamPerfect).r;
+				float finalColorG = textureGradient(envMap, rayDirectionG, directionCamPerfect).g;
+				float finalColorB = textureGradient(envMap, rayDirectionB, directionCamPerfect).b;
+				diffuseColor.rgb *= vec3(finalColorR, finalColorG, finalColorB);
+			  #else
+				rayDirection = totalInternalReflection(rayOrigin, rayDirection, normal, max(ior, 1.0), vModelMatrixInverse);
+				diffuseColor.rgb *= textureGradient(envMap, rayDirection, directionCamPerfect).rgb;
+			  #endif
+		  
+			  vec3 viewDirection = normalize(vWorldPosition - cameraPosition);
+
+			  float nFresnel = fresnelFunc(viewDirection, normal) * fresnel;
+			  gl_FragColor = vec4(mix(diffuseColor.rgb, vec3(1.0), nFresnel), 1.0);
+
+			  #include <tonemapping_fragment>
+			  #include <${169 >= 154 ? 'colorspace_fragment' : 'encodings_fragment'}>
+			}`
 		});
 	}
 }
