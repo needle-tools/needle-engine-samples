534ac49c024e395053db7bf93210081bd449c43d
Marcel Wiessler <marcel@gaisterhand.de>
2025-10-09 17:28:40 +0200

    Start see through walls sample (needs cleanup since we re-used materials from configurator)

---
+++ package/Runtime/_Shared/SampleScripts.codegen/SeeThroughFade.cs
@@ -0,0 +1,18 @@
+// NEEDLE_CODEGEN_START
+// auto generated code - do not edit directly
+
+#pragma warning disable
+
+namespace Needle.Typescript.GeneratedComponents
+{
+	public partial class SeeThroughFade : UnityEngine.MonoBehaviour
+	{
+		public UnityEngine.GameObject @center;
+		public float @fadeDuration = 0.05f;
+		public float @minAlpha = 0f;
+		public bool @useAlphaHash = true;
+		public void update(){}
+	}
+}
+
+// NEEDLE_CODEGEN_END
